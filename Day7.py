1f59750ed4f405bdd4c045b5ed07c5811d2388bc
Mike Stark <stark3@gmail.com>
2015-12-10 15:14:38 -0500

    Finished Day 7 part 1 and 2

--- Day7.py
+++ Day7.py
@@ -1,118 +1,100 @@
 import os
 import re
 
-def do_assign(circuit, wire_in, wire_out):
-  val = 0
-  try:
-    val = int(wire_in)
-  except:
-    if wire_in not in circuit:
-      circuit[wire_in] = 0
-    val = circuit[wire_in]
-
-  circuit[wire_out] = val
-
-def do_not(circuit, wire_in, wire_out):
-  if wire_in not in circuit:
-    circuit[wire_in] = 0
-  circuit[wire_out] = (~circuit[wire_in]) % (65536)
-
-def do_binary(circuit, gate, wire1, wire2, wire_out):
-  val1 = 0
-  try:
-    val1 = int(wire1)
-  except:
-    if wire1 not in circuit:
-      circuit[wire1] = 0
-    val1 = circuit[wire1]
-
-  val2 = 0
-  try:
-    val1 = int(wire2)
-  except:
-    if wire2 not in circuit:
-      circuit[wire2] = 0
-    val1 = circuit[wire2]
-  
-  val = 0
-  if gate == 'AND':
-    val = val1 & val2
-  elif gate == 'OR':
-    val = val1 | val2
-
-  circuit[wire_out] = val
-
-def do_shift(circuit, wire_in, shifter, amount, wire_out):
-  if wire_in not in circuit:
-    circuit[wire_in] = 0
-
-  val = 0
-  if shifter == 'LSHIFT':
-    val = circuit[wire_in] << amount
-  elif shifter == 'RSHIFT':
-    val = circuit[wire_in] >> amount
-
-  circuit[wire_out] = val
-
-def build_circuit1(directions):
+def read_directions(directions):
   circuit = dict()
-  number_pattern = re.compile(r'^(\d+|[a-z]+) -> ([a-z]+)$', re.IGNORECASE)
-  unary_pattern = re.compile(r'^NOT ([a-z]+) -> ([a-z]+)$', re.IGNORECASE)
-  binary_pattern = re.compile(r'^(\d+|[a-z]+) (AND|OR) (\d+|[a-z]+) -> ([a-z]+)$', re.IGNORECASE)
-  shift_pattern = re.compile(r'^([a-z]+) (LSHIFT|RSHIFT) (\d+) -> ([a-z]+)$', re.IGNORECASE)
-
-  counter = 0
+  direction_pattern = re.compile(r'^(.*) -> ([a-z]+)$', re.IGNORECASE)
 
   for d in directions:
-    if counter == 5:
-      break
-    counter = counter + 1
-    line = d.rstrip()
-    number_match = number_pattern.match(line)
-    unary_match = unary_pattern.match(line)
-    binary_match = binary_pattern.match(line)
-    shift_match = shift_pattern.match(line)
+    m = direction_pattern.match(d)
+    input_calc = m.group(1)
+    result_wire = m.group(2)
+    circuit[result_wire] = input_calc
 
-    print("%s, %s %s %s %s" % (line, number_match, unary_match, binary_match, shift_match))
+  return circuit
 
-    if number_match:
-      wire_in = number_match.group(1)
-      wire_out = number_match.group(2)
+number_pattern = re.compile(r'^(\d+|[a-z]+)$', re.IGNORECASE)
+unary_pattern = re.compile(r'^NOT ([a-z]+)$', re.IGNORECASE)
+binary_pattern = re.compile(r'^(\d+|[a-z]+) (AND|OR) (\d+|[a-z]+)$', re.IGNORECASE)
+shift_pattern = re.compile(r'^([a-z]+) (LSHIFT|RSHIFT) (\d+)$', re.IGNORECASE)
 
-      do_assign(circuit, wire_in, wire_out)
+def evaluate_circuit(circuit, wire):
+  result = 0
+  calc = circuit[wire]
+  try:
+    # Has it already been evaluated?
+    result = int(calc)
+    return result
+  except:
+    # There was a problem: we need to evaluate
+    pass
 
-    elif unary_match:
-      wire_in = unary_match.group(1)
-      wire_out = unary_match.group(2)
+  number_match = number_pattern.match(calc)
+  unary_match = unary_pattern.match(calc)
+  binary_match = binary_pattern.match(calc)
+  shift_match = shift_pattern.match(calc)
 
-      do_not(circuit, wire_in, wire_out)
+  if number_match:
+    try:
+      result = int(calc)
+    except:
+      result = evaluate_circuit(circuit, calc)
 
-    elif binary_match:
-      wire1 = binary_match.group(1)
-      gate = binary_match.group(2)
-      wire2 = binary_match.group(3)
-      wire_out = binary_match.group(4)
+  elif unary_match:
+    input_wire = unary_match.group(1)
+    result = ~evaluate_circuit(circuit, input_wire)
 
-      do_binary(circuit, gate, wire1, wire2, wire_out)
+  elif binary_match:
+    wire1 = binary_match.group(1)
+    gate = binary_match.group(2)
+    wire2 = binary_match.group(3)
 
-    elif shift_match:
-      wire_in = shift_match.group(1)
-      shifter = shift_match.group(2)
-      amount = int(shift_match.group(3))
-      wire_out = shift_match.group(4)
+    val1 = 0
+    try:
+      val1 = int(wire1)
+    except:
+      val1 = evaluate_circuit(circuit, wire1)
 
-      do_shift(circuit, wire_in, shifter, amount, wire_out)
+    val2 = 0
+    try:
+      val2 = int(wire2)
+    except:
+      val2 = evaluate_circuit(circuit, wire2)
 
+    if gate == 'AND':
+      result = val1 & val2
     else:
-      print("Bad command found: '%s'" % line)
+      result = val1 | val2
 
-  #print("Circuit emulation complete: wire a has value %d" % circuit['a'])
-  for c in circuit:
-    print("\t%s: %d" % (c, circuit[c]))
+  elif shift_match:
+    wire1 = shift_match.group(1)
+    shifter = shift_match.group(2)
+    amt = int(shift_match.group(3))
+
+    val = evaluate_circuit(circuit, wire1)
+    if shifter == 'LSHIFT':
+      result = val << amt
+    else:
+      result = val >> amt
+
+  circuit[wire] = int(result)
+  return circuit[wire] 
+
+def build_circuit1(directions):
+  circuit = read_directions(directions)
+
+  old_direction = circuit['a']
+  a_val = evaluate_circuit(circuit, 'a')
+  print("Part 1: Wire a has value %d" % a_val)
+
+  circuit = read_directions(directions)
+  circuit['b'] = a_val
+  a_val = evaluate_circuit(circuit, 'a')
+  print("Part 2: Wire a has value %d" % a_val)
 
 def main():
   f = open('Day7Input.txt', 'r')
-  directions = f.readlines()
+  directions = [l.rstrip() for l in f.readlines()]
   build_circuit1(directions)
 
 if __name__ == "__main__":
