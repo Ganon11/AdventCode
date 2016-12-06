cee33a8372f1774ffb53b7cc421382c83b9b14ac
Ganon11 <stark3@gmail.com>
2016-12-06 10:52:27 -0500

    Adding Perl impl for Day 6 parts 1 and 2.

---
+++ 2016/Day6/Day6.pl
@@ -0,0 +1,63 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+sub ReadFile {
+  my $inputfile = shift;
+  open(my $fh, '<', $inputfile) or die "Couldn't open $inputfile: $!";
+
+  my @counts = ();
+  while (my $line = <$fh>) {
+    $line =~ s/\R//g; # Trim line breaks.
+    my @chars = split(//, $line);
+    my $charsize = scalar(@chars);
+    for (my $i = 0; $i < $charsize; $i = $i + 1) {
+      my $result = $counts[$i]{$chars[$i]};
+      if (defined($result)) {
+        $counts[$i]{$chars[$i]} = $result + 1;
+      } else {
+        $counts[$i]{$chars[$i]} = 1;
+      }
+    }
+  }
+
+  close($fh);
+
+  return @counts;
+}
+
+sub RestoreStringHighestFrequency {
+  my $aref = shift;
+  my @counts = @{$aref};
+  my $string = '';
+
+  foreach my $hashref (@counts) {
+    my @sortedChars = sort { $hashref->{$b} <=> $hashref->{$a} } keys %$hashref;
+    $string = $string . $sortedChars[0];
+  }
+
+  return $string;
+}
+
+sub RestoreStringLowestFrequency {
+  my $aref = shift;
+  my @counts = @{$aref};
+  my $string = '';
+
+  foreach my $hashref (@counts) {
+    my @sortedChars = sort { $hashref->{$a} <=> $hashref->{$b} } keys %$hashref;
+    $string = $string . $sortedChars[0];
+  }
+
+  return $string;
+}
+
+my $inputfile = shift;
+if (!defined($inputfile)) {
+  $inputfile = 'Sample.txt'
+}
+
+my @counts = ReadFile($inputfile);
+print RestoreStringHighestFrequency(\@counts), "\n";
+print RestoreStringLowestFrequency(\@counts), "\n";
