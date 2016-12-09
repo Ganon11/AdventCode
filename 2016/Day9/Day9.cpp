084b3d297a02afc0cc7bd0edf1bd68d8f7abc901
Ganon11 <stark3@gmail.com>
2016-12-09 15:09:03 -0500

    Adding CPP solution.

--- 2016/Day9/Day9.cpp
+++ 2016/Day9/Day9.cpp
@@ -4,8 +4,68 @@
 #include "stdafx.h"
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <vector>
+
+std::wstring GetCompressedString(const std::wstring& inputFile) {
+   std::wstring str;
+   std::wifstream input{ inputFile };
+   if (!input.is_open()) {
+      return str;
+   }
+
+   do {
+      std::wstring line;
+      std::getline(input, line);
+      str.append(line);
+   } while (input.good());
+
+   return str;
+}
+
+template <int> size_t Decompress(const std::wstring& input);
+
+template <> size_t Decompress<1>(const std::wstring& input) {
+   size_t pos{ input.find(L'(') };
+   if (pos == std::wstring::npos) {
+      return input.length();
+   }
+   else {
+      std::wregex re{ L"\\((\\d+)x(\\d+)\\)" };
+      std::wsmatch m;
+
+      if (!std::regex_search(input, m, re)) {
+         return 0;
+      }
+
+      std::wstring bracket{ m[0] };
+      size_t size{ static_cast<size_t>(_wtoi(m[1].str().c_str())) }, repeat{ static_cast<size_t>(_wtoi(m[2].str().c_str())) };
+      size_t index{ pos + bracket.length() };
+      return pos + (size * repeat) + Decompress<1>(input.substr(index + size));
+   }
+}
+
+template <> size_t Decompress<2>(const std::wstring& input) {
+   size_t pos{ input.find(L'(') };
+   if (pos == std::wstring::npos) {
+      return input.length();
+   }
+   else {
+      std::wregex re{ L"\\((\\d+)x(\\d+)\\)" };
+      std::wsmatch m;
+
+      if (!std::regex_search(input, m, re)) {
+         return 0;
+      }
+
+      std::wstring bracket{ m[0] };
+      size_t size{ static_cast<size_t>(_wtoi(m[1].str().c_str())) }, repeat{ static_cast<size_t>(_wtoi(m[2].str().c_str())) };
+      size_t index{ pos + bracket.length() };
+      std::wstring textToRepeat{ input.substr(index, size) };
+      return pos + (Decompress<2>(textToRepeat) * repeat) + Decompress<2>(input.substr(index + size));
+   }
+}
 
 int wmain(int argc, wchar_t *argv[]) {
    std::wstring inputFile;
@@ -13,8 +73,13 @@ int wmain(int argc, wchar_t *argv[]) {
       inputFile = argv[1];
    }
    else {
-      inputFile = L"Sample.txt";
+      inputFile = L"Sample2.txt";
    }
+
+   std::wstring compressedText{ GetCompressedString(inputFile) };
+
+   std::wcout << L"First: " << Decompress<1>(compressedText) << std::endl;
+   std::wcout << L"Second: " << Decompress<2>(compressedText) << std::endl;
 
    return 0;
 }
