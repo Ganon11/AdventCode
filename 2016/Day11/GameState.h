b884bd69a22b4623f86c5ffd8d5c48096ee62955
Mike Stark <stark3@gmail.com>
2016-12-12 13:36:14 -0500

    More work on optimizing Day 11. It's fast! It's just wrong.

--- 2016/Day11/GameState.h
+++ 2016/Day11/GameState.h
@@ -1,15 +1,21 @@
 #pragma once
 
-#include "Floor.h"
+//#include "Floor.h"
+#include "ObjectPair.h"
 #include <vector>
 
 class GameState {
 public:
-   GameState(const unsigned int floor, const FloorState& floorState, const unsigned int moves);
+   //GameState(const unsigned int floor, const FloorState& floorState, const unsigned int moves);
+
+   GameState(const unsigned int floor, const ObjectState& objectState, const unsigned int moves);
 
    unsigned int GetCurrentFloor() const;
-   FloorState GetFloorState() const;
+   //FloorState GetFloorState() const;
+   ObjectState GetObjectState() const;
    unsigned int GetMoveCount() const;
+
+   unsigned int GetFloorObjectCount(const unsigned int floor) const;
    bool IsFinalState() const;
 
    bool operator==(const GameState& rhs) const;
@@ -18,14 +24,16 @@ public:
 
 private:
    unsigned int m_currentFloor;
-   FloorState m_floorState;
+   //FloorState m_floorState;
+   ObjectState m_objectState;
    unsigned int m_moves;
 };
 
 namespace std {
    template<> struct hash<GameState> {
       size_t operator()(const GameState& gs) const noexcept {
-         return (gs.GetCurrentFloor() + 1) * std::hash<FloorState>()(gs.GetFloorState());
+         //return (gs.GetCurrentFloor() + 1) * std::hash<FloorState>()(gs.GetFloorState());
+         return (gs.GetCurrentFloor() + 1) * std::hash<ObjectState>()(gs.GetObjectState());
       }
    };
 }
