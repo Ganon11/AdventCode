924f0554c68972f179289b92099d94c78b327dbd
Michael Stark <stark3@gmail.com>
2016-12-23 09:59:28 -0500

    Tweaking day 21 and 22.

--- 2016/Day22/Day22.pl
+++ 2016/Day22/Day22.pl
@@ -1,13 +1,10 @@
 use strict;
 use warnings;
 
-use Data::Dumper;
-
 my @nodes;
 open(my $fh, '<', 'Input.txt');
 foreach my $line (<$fh>) {
   $line =~ s/\R//g;
-  # /dev/grid/node-x0-y0     89T   67T    22T   75%
   if ($line =~ m/^\/dev\/grid\/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%$/) {
     my ($x, $y, $size, $used, $avail, $usePercent) = (int($1), int($2), int($3), int($4), int($5), int($6));
     $nodes[$x][$y] = {
@@ -17,14 +14,8 @@ foreach my $line (<$fh>) {
       'use%' => $usePercent
     };
   }
-  else {
-    # Skip
-    next;
-  }
 }
 close($fh);
-
-#print Dumper \@nodes;
 
 my @viable;
 my $rowCount = scalar(@nodes);
@@ -38,7 +29,7 @@ for (my $x1 = 0; $x1 < $rowCount; ++$x1) {
 
         my $B = $nodes[$x2][$y2];
         if ($A->{'used'} > 0 && $A->{'used'} < $B->{'avail'}) {
-          push(@viable, [$x1, $y1, $x2, $y2]);
+          push(@viable, "($x1, $y1) -> ($x2, $y2)");
         }
       }
     }
@@ -48,7 +39,16 @@ for (my $x1 = 0; $x1 < $rowCount; ++$x1) {
 my $numViable = scalar(@viable);
 print "Viable nodes: $numViable\n";
 
-print '  ';
+# First line - 10s place
+print '   ';
+for (my $c = 0; $c < $rowCount; ++$c) {
+  my $legend = int($c / 10);
+  print "$legend ";
+}
+print "\n";
+
+# Second line - 1s place
+print '   ';
 for (my $c = 0; $c < $rowCount; ++$c) {
   my $legend = ($c % 10);
   print "$legend ";
@@ -56,8 +56,8 @@ for (my $c = 0; $c < $rowCount; ++$c) {
 print "\n";
 
 for (my $y = 0; $y < $columnCount; ++$y) {
-  my $legend = ($y % 10);
-  print "$legend ";
+  print ' ' if $y < 10;
+  print "$y ";
   for (my $x = 0; $x < $rowCount; ++$x) {
     if ($x == $rowCount - 1 && $y == 0) {
       print 'G ';
@@ -75,8 +75,3 @@ for (my $y = 0; $y < $columnCount; ++$y) {
 
   print "\n";
 }
-
-# 26 to get into position
-# 5 per vertical move * 33 vertical moves = 165
-# 1 for final move
-# Total: 192
