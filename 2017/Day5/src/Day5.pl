3815355bf3f178b81fd2cadff6f82dfc6505669a
Mike Stark <mstark@factset.com>
2017-12-05 09:48:14 -0500

    Really adding day 5 solution.

--- 2017/Day5/src/Day5.pl
+++ 2017/Day5/src/Day5.pl
@@ -1,6 +1,24 @@
 use strict;
 use warnings;
 
+sub CalculateSteps {
+  my $instructionsRef = shift;
+  my $newOffsetFunc = shift;
+  my @instructions = @{$instructionsRef};
+
+  my $instructionPointer = 0;
+  my $steps = 0;
+  while ($instructionPointer >= 0 and $instructionPointer < scalar(@instructions)) {
+    my $oldInstruction = $instructionPointer;
+    my $offset = $instructions[$oldInstruction];
+    $instructionPointer = $instructionPointer + $instructions[$oldInstruction];
+    $instructions[$oldInstruction] = &$newOffsetFunc($offset);
+    $steps = $steps + 1;
+  }
+
+  return $steps;
+}
+
 my $inputFile;
 if (scalar(@ARGV) > 0) {
   $inputFile = shift @ARGV;
@@ -17,3 +35,18 @@ while (<$fh>) {
 }
 close($fh);
 
+my $part1 = CalculateSteps(\@instructions, sub {
+  my $offset = shift;
+  return $offset + 1;
+});
+print "Part 1: Made it out in $part1 steps\n";
+
+my $part2 = CalculateSteps(\@instructions, sub {
+  my $offset = shift;
+  if ($offset >= 3) {
+    return $offset - 1;
+  } else {
+    return $offset + 1;
+  }
+});
+print "Part 2: Made it out in $part2 steps\n";
