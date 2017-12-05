34fd8bebdf1609ae9dbc470fac7f3f00c5db6c56
Mike Stark <mstark@factset.com>
2017-12-05 09:47:39 -0500

    Adding Day 5 solution.

--- 2017/Day5/src/Day5.pl
+++ 2017/Day5/src/Day5.pl
@@ -8,8 +8,12 @@ if (scalar(@ARGV) > 0) {
   $inputFile = '../input/Input.txt';
 }
 
+my @instructions = ();
 open(my $fh, "<", $inputFile) or die "Couldn't open $inputFile: $!";
 while (<$fh>) {
   my $line = $_;
   chomp($line);
+  push(@instructions, int($line));
 }
+close($fh);
+
