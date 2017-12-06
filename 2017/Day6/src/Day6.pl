f2dc0f1044532d6994e9da5bec10f4f3d184ca8a
Mike Stark <mstark@factset.com>
2017-12-06 09:35:57 -0500

    Adding Day 6 solution. Modified sublime-project for just one input file.

--- 2017/Day6/src/Day6.pl
+++ 2017/Day6/src/Day6.pl
@@ -8,8 +8,13 @@ if (scalar(@ARGV) > 0) {
   $inputFile = '../input/Input.txt';
 }
 
+my @banks = ();
 open(my $fh, "<", $inputFile) or die "Couldn't open $inputFile: $!";
 while (<$fh>) {
   my $line = $_;
   chomp($line);
+  @banks = map { int($_) } split(/\s+/, $line);
 }
+close($fh);
+
+print "Banks: @banks\n";
