07dea1ff428c9d469940482810dcf51a185e0e95
Ganon11 <stark3@gmail.com>
2017-12-10 15:36:12 -0500

    Part 1 of Day 10.

--- 2017/Day10/src/Day10.pl
+++ 2017/Day10/src/Day10.pl
@@ -1,6 +1,23 @@
 use strict;
 use warnings;
 
+sub ReverseSection {
+  my $listRef = shift;
+  my $start = shift;
+  my $length = shift;
+
+  my $listLength = scalar(@$listRef);
+  my @reversedList = ();
+  for (my $i = 0; $i < $length; ++$i) {
+    push(@reversedList, $$listRef[($start + $i) % $listLength]);
+  }
+
+  @reversedList = reverse(@reversedList);
+  for (my $i = 0; $i < $length; ++$i) {
+    $$listRef[($start + $i) % $listLength] = $reversedList[$i];
+  }
+}
+
 my $inputFile;
 if (scalar(@ARGV) > 0) {
   $inputFile = shift @ARGV;
@@ -9,7 +26,29 @@ if (scalar(@ARGV) > 0) {
 }
 
 open(my $fh, "<", $inputFile) or die "Couldn't open $inputFile: $!";
-while (<$fh>) {
-  my $line = $_;
-  chomp($line);
+my $line = <$fh>;
+chomp($line);
+close($fh);
+my @lengths = map { int($_) } split(/,/, $line);
+
+my @list;
+if ($inputFile =~ m/Sample/i) {
+  @list = (0..4);
+} else {
+  @list = (0..255);
 }
+my $listLength = scalar(@list);
+
+my $position = 0;
+my $skipSize = 0;
+
+foreach my $length (@lengths) {
+#  print "List: @list, position: $position, skip size: $skipSize, length: $length\n";
+  ReverseSection(\@list, $position, $length);
+  $position = ($position + $length + $skipSize) % $listLength;
+  $skipSize = $skipSize + 1;
+}
+
+#print "List: @list\n";
+my $product = $list[0] * $list[1];
+print "Part 1: $product\n";
