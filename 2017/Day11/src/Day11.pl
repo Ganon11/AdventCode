8728c03475f7bdc39d87945b679fc4637e69685d
Ganon11 <stark3@gmail.com>
2017-12-11 09:54:25 -0500

    Hex grids forever!

--- 2017/Day11/src/Day11.pl
+++ 2017/Day11/src/Day11.pl
@@ -1,5 +1,13 @@
 use strict;
 use warnings;
+use Switch;
+
+sub CubeDistance {
+  my ($x, $y, $z) = @_;
+  # return (abs(a.x - b.x) + abs(a.y - b.y) + abs(a.z - b.z)) / 2
+  # All B coordinates will be 0, which reduces this to:
+  return (abs($x) + abs($y) + abs($z)) / 2;
+}
 
 my $inputFile;
 if (scalar(@ARGV) > 0) {
@@ -9,7 +17,48 @@ if (scalar(@ARGV) > 0) {
 }
 
 open(my $fh, "<", $inputFile) or die "Couldn't open $inputFile: $!";
-while (<$fh>) {
-  my $line = $_;
-  chomp($line);
+my $line = <$fh>;
+chomp($line);
+close($fh);
+
+my @directions = split(/,/, $line);
+
+my ($x, $y, $z) = (0, 0, 0);
+my $maxDistance = 0;
+foreach my $direction (@directions) {
+  switch ($direction) {
+    case 'n' {
+      ++$y;
+      --$z;
+    }
+    case 'ne' {
+      ++$x;
+      --$z;
+    }
+    case 'se' {
+      --$y;
+      ++$x;
+    }
+    case 's' {
+      --$y;
+      ++$z;
+    }
+    case 'sw' {
+      --$x;
+      ++$z;
+    }
+    case 'nw' {
+      ++$y;
+      --$x;
+    }
+    else {
+      die "Unknown direction $direction";
+    }
+  }
+
+  my $tempDistance = CubeDistance($x, $y, $z);
+  $maxDistance = $tempDistance if ($tempDistance > $maxDistance);
 }
+
+my $distance = CubeDistance($x, $y, $z);
+print "Location: ($x, $y, $z), distance $distance, max distance $maxDistance\n";
