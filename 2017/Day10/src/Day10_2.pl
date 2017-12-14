540f094e758749c50a1b3dc94d3cafca64111cb6
Ganon11 <stark3@gmail.com>
2017-12-14 10:10:39 -0500

    Turn Knot Hash into a function (I used this in Day 14).

--- 2017/Day10/src/Day10_2.pl
+++ 2017/Day10/src/Day10_2.pl
@@ -18,6 +18,37 @@ sub ReverseSection {
   }
 }
 
+sub CalculateKnotHash {
+  my $input = shift;
+
+  my @lengths = map { ord($_) } split(//, $input);
+  push(@lengths, 17, 31, 73, 47, 23);
+  my @list = (0..255);
+  my $listLength = scalar(@list);
+  my $position = 0;
+  my $skipSize = 0;
+
+  foreach my $round (1..64) {
+    foreach my $length (@lengths) {
+      ReverseSection(\@list, $position, $length);
+      $position = ($position + $length + $skipSize) % $listLength;
+      $skipSize = $skipSize + 1;
+    }
+  }
+
+  my $denseHashString = '';
+  foreach my $digit (0..15) {
+    my $start = $digit * 16;
+    my $result = $list[$start];
+    foreach my $offset (1..15) {
+      $result = $result ^ $list[$start + $offset];
+    }
+    $denseHashString = $denseHashString . sprintf("%02X", $result);
+  }
+
+  return $denseHashString;
+}
+
 my $inputFile;
 if (scalar(@ARGV) > 0) {
   $inputFile = shift @ARGV;
@@ -30,44 +61,5 @@ my $line = <$fh>;
 chomp($line);
 close($fh);
 
-
-my @lengths = map { ord($_) } split(//, $line);
-push(@lengths, 17, 31, 73, 47, 23);
-print "ASCII codes: @lengths\n";
-
-my @list;
-if ($inputFile =~ m/Sample/i) {
-  @list = (0..4);
-} else {
-  @list = (0..255);
-}
-my $listLength = scalar(@list);
-
-my $position = 0;
-my $skipSize = 0;
-
-foreach my $round (1..64) {
-  foreach my $length (@lengths) {
-    ReverseSection(\@list, $position, $length);
-    $position = ($position + $length + $skipSize) % $listLength;
-    $skipSize = $skipSize + 1;
-  }
-}
-
-print "List so far: @list\n";
-
-# Assemble dense hash
-my @denseHash;
-my $denseHashString = '';
-foreach my $digit (0..15) {
-  my $start = $digit * 16;
-  my $result = $list[$start];
-  foreach my $offset (1..15) {
-    $result = $result ^ $list[$start + $offset];
-  }
-  push(@denseHash, $result);
-  $denseHashString = $denseHashString . sprintf("%02X", $result);
-}
-
-print "Dense hash (decimal): @denseHash\n";
-print "Dense hash: $denseHashString\n";
+my $knotHash = CalculateKnotHash($line);
+print "Knot hash: $knotHash\n";
