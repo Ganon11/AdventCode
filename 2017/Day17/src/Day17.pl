f5f44897d13691574c0262811e525e04237ea698
Michael Stark <stark3@gmail.com>
2017-12-17 20:06:05 -0500

    Part 2 for day 17. Part 1 is buried in there somewhere.

--- 2017/Day17/src/Day17.pl
+++ 2017/Day17/src/Day17.pl
@@ -8,8 +8,30 @@ if (scalar(@ARGV) > 0) {
   $inputFile = '../input/Input.txt';
 }
 
+my $steps;
 open(my $fh, "<", $inputFile) or die "Couldn't open $inputFile: $!";
-while (<$fh>) {
-  my $line = $_;
-  chomp($line);
+my $line = <$fh>;
+chomp($line);
+$steps = int($line);
+close($fh);
+
+my @buffer = ( 0 ) x 1;
+my $position = 0;
+
+my $afterValue;
+foreach my $size (1..50000000) {
+  $position = ($position + $steps) % $size;
+  if ($position == 0) {
+    $afterValue = $size;
+  }
+  #splice(@buffer, $position + 1, 0, $newValue);
+  ++$position;
+
+  #print "@buffer\n";
 }
+
+#print "@buffer\n";
+#print "buffer[$position + 2] = $buffer[$position+2]\n";
+#print "buffer[1] = $buffer[1]\n";
+
+print "$afterValue\n";
