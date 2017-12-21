e733286605b3993c49072fab906a4024b3533218
Mike Stark <mstark@factset.com>
2017-12-21 14:58:41 -0500

    Cleanup.

--- 2017/Day21/src/Day21.pl
+++ 2017/Day21/src/Day21.pl
@@ -2,67 +2,6 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-sub CountPixels {
-  my $patternRef = shift;
-  my $size = scalar(@$patternRef);
-
-  my $total = 0;
-  foreach my $r (0..($size - 1)) {
-    foreach my $c (0..($size - 1)) {
-      ++$total if $$patternRef[$r][$c] eq '#';
-    }
-  }
-
-  return $total;
-}
-
-sub PrintPattern {
-  my $patternRef = shift;
-  PrintPatternString(TranslateSquareToString($patternRef, scalar(@$patternRef), 0, 0));
-}
-
-sub PrintPatternString {
-  my $string = shift;
-  foreach my $row (split(/\//, $string)) {
-    print "$row\n";
-  }
-  print "\n";
-}
-
-sub TranslateSquareToString {
-  my $patternRef = shift;
-  my $size = shift;
-  my $x = shift;
-  my $y = shift;
-
-  my @rowStrings;
-  foreach my $r (0..($size - 1)) {
-    my $rowString = '';
-    foreach my $c (0..($size - 1)) {
-      $rowString .= $$patternRef[$r + $x][$c + $y];
-    }
-    push(@rowStrings, $rowString);
-  }
-
-  return join('/', @rowStrings);
-}
-
-sub TranslateStringToSquare {
-  my $string = shift;
-
-  my @rows = split(/\//, $string);
-  my $size = scalar(@rows);
-  my $pattern = [];
-  foreach my $r (0..($size - 1)) {
-    my @characters = split(//, $rows[$r]);
-    foreach my $c (0..($size - 1)) {
-      $$pattern[$r][$c] = $characters[$c];
-    }
-  }
-
-  return $pattern;
-}
-
 sub RotatePattern {
   my $patternRef = shift;
   my $size = scalar(@$patternRef);
@@ -143,6 +82,140 @@ sub FlipString {
   return TranslateSquareToString($pattern, scalar(@$pattern), 0, 0);
 }
 
+sub AddMatch {
+  my $patterns = shift;
+  my $line = shift;
+
+  $line =~ m/^(\S+) => (\S+)$/i;
+  my ($match, $expansion) = ($1, $2);
+  $patterns->{$match} = $expansion;
+
+  my $flipped = FlipString($match);
+  $patterns->{$flipped} = $expansion;
+
+  $match = RotateString($match);
+  $patterns->{$match} = $expansion;
+
+  $flipped = FlipString($match);
+  $patterns->{$flipped} = $expansion;
+
+  $match = RotateString($match);
+  $patterns->{$match} = $expansion;
+
+  $flipped = FlipString($match);
+  $patterns->{$flipped} = $expansion;
+
+  $match = RotateString($match);
+  $patterns->{$match} = $expansion;
+
+  $flipped = FlipString($match);
+  $patterns->{$flipped} = $expansion;
+}
+
+sub CountPixels {
+  my $patternRef = shift;
+  my $size = scalar(@$patternRef);
+
+  my $total = 0;
+  foreach my $r (0..($size - 1)) {
+    foreach my $c (0..($size - 1)) {
+      ++$total if $$patternRef[$r][$c] eq '#';
+    }
+  }
+
+  return $total;
+}
+
+sub TranslateSquareToString {
+  my $patternRef = shift;
+  my $size = shift;
+  my $x = shift;
+  my $y = shift;
+
+  my @rowStrings;
+  foreach my $r (0..($size - 1)) {
+    my $rowString = '';
+    foreach my $c (0..($size - 1)) {
+      $rowString .= $$patternRef[$r + $x][$c + $y];
+    }
+    push(@rowStrings, $rowString);
+  }
+
+  return join('/', @rowStrings);
+}
+
+sub TranslateStringToSquare {
+  my $string = shift;
+
+  my @rows = split(/\//, $string);
+  my $size = scalar(@rows);
+  my $pattern = [];
+  foreach my $r (0..($size - 1)) {
+    my @characters = split(//, $rows[$r]);
+    foreach my $c (0..($size - 1)) {
+      $$pattern[$r][$c] = $characters[$c];
+    }
+  }
+
+  return $pattern;
+}
+
+sub ExpandTwoByTwo {
+  my $grid = shift;
+  my $patterns = shift;
+
+  my $size = scalar(@$grid);
+  my $blockCount = $size / 2;
+  my @newGrid;
+  for (my $i = 0; $i < $blockCount; ++$i) {
+    for (my $j = 0; $j < $blockCount; ++$j) {
+      # Grab a 2x2 block starting at (2i, 2j) and ending at (2i+1, 2j+1)
+      my $inputString = TranslateSquareToString($grid, 2, (2 * $i), (2 * $j));
+      my $expandedString = $patterns->{$inputString};
+      my @block = @{ TranslateStringToSquare($expandedString) };
+
+      # @block is a 3x3 square that will end up in @newGrid at (3i, 3j) through (3i+2, 3j+2)
+      foreach my $blockRow (0..2) {
+        foreach my $blockCol (0..2) {
+          $newGrid[(3 * $i) + $blockRow][(3 * $j) + $blockCol] = $block[$blockRow][$blockCol];
+        }
+      }
+    }
+  }
+
+  return @newGrid;
+}
+
+sub ExpandThreeByThree {
+  my $grid = shift;
+  my $patterns = shift;
+
+  my $size = scalar(@$grid);
+  my $blockCount = $size / 3;
+  my @newGrid;
+  for (my $i = 0; $i < $blockCount; ++$i) {
+    for (my $j = 0; $j < $blockCount; ++$j) {
+      # Grab a 3x3 block starting at (3i, 3j) and ending at (3i+2, 3j+2)
+      my $inputString = TranslateSquareToString($grid, 3, (3 * $i), (3 * $j));
+      my $expandedString = $patterns->{$inputString};
+      my @block = @{ TranslateStringToSquare($expandedString) };
+
+      # @block is a 4x4 square that will end up in @newGrid at (4i, 4j) through (4i+3, 4j+3)
+      foreach my $blockRow (0..3) {
+        foreach my $blockCol (0..3) {
+          $newGrid[(4 * $i) + $blockRow][(4 * $j) + $blockCol] = $block[$blockRow][$blockCol];
+        }
+      }
+    }
+  }
+
+  return @newGrid;
+}
+
+################################################################################
+#                                MAIN PROG                                     #
+################################################################################
+
 my $inputFile;
 if (scalar(@ARGV) > 0) {
   $inputFile = shift @ARGV;
@@ -155,38 +228,7 @@ open(my $fh, "<", $inputFile) or die "Couldn't open $inputFile: $!";
 while (<$fh>) {
   my $line = $_;
   chomp($line);
-  $line =~ m/^(\S+) => (\S+)$/i;
-  my ($match, $expansion) = ($1, $2);
-  #print "Adding $match => $expansion\n";
-  $patterns{$match} = $expansion;
-
-  my $flipped = FlipString($match);
-  #print "Adding $flipped => $expansion\n";
-  $patterns{$flipped} = $expansion;
-
-  $match = RotateString($match);
-  #print "Adding $match => $expansion\n";
-  $patterns{$match} = $expansion;
-
-  $flipped = FlipString($match);
-  #print "Adding $flipped => $expansion\n";
-  $patterns{$flipped} = $expansion;
-
-  $match = RotateString($match);
-  #print "Adding $match => $expansion\n";
-  $patterns{$match} = $expansion;
-
-  $flipped = FlipString($match);
-  #print "Adding $flipped => $expansion\n";
-  $patterns{$flipped} = $expansion;
-
-  $match = RotateString($match);
-  #print "Adding $match => $expansion\n";
-  $patterns{$match} = $expansion;
-
-  $flipped = FlipString($match);
-  #print "Adding $flipped => $expansion\n";
-  $patterns{$flipped} = $expansion;
+  AddMatch(\%patterns, $line);
 }
 close($fh);
 
@@ -198,51 +240,13 @@ my $iterations = 0;
 my $TOTAL_ITERATIONS = 18;
 
 do {
-  # PrintPattern(\@grid);
-  my $size = scalar(@grid);
-
-  my @newGrid;
-  if (0 == ($size % 2)) {
-    # Split into 2x2 blocks
-    my $blockCount = $size / 2;
-    for (my $i = 0; $i < $blockCount; ++$i) {
-      for (my $j = 0; $j < $blockCount; ++$j) {
-        # Grab a 2x2 block starting at (2i, 2j) and ending at (2i+1, 2j+1)
-        my $inputString = TranslateSquareToString(\@grid, 2, (2 * $i), (2 * $j));
-        my $expandedString = $patterns{$inputString};
-        my @block = @{ TranslateStringToSquare($expandedString) };
-
-        # @block is a 3x3 square that will end up in @newGrid at (3i, 3j) through (3i+2, 3j+2)
-        foreach my $blockRow (0..2) {
-          foreach my $blockCol (0..2) {
-            $newGrid[(3 * $i) + $blockRow][(3 * $j) + $blockCol] = $block[$blockRow][$blockCol];
-          }
-        }
-      }
-    }
+  if (0 == (scalar(@grid) % 2)) {
+    @grid = ExpandTwoByTwo(\@grid, \%patterns);
   } else {
-    # Split into 3x3 blocks
-    my $blockCount = $size / 3;
-    for (my $i = 0; $i < $blockCount; ++$i) {
-      for (my $j = 0; $j < $blockCount; ++$j) {
-        # Grab a 3x3 block starting at (3i, 3j) and ending at (3i+2, 3j+2)
-        my $inputString = TranslateSquareToString(\@grid, 3, (3 * $i), (3 * $j));
-        my $expandedString = $patterns{$inputString};
-        my @block = @{ TranslateStringToSquare($expandedString) };
-
-        # @block is a 4x4 square that will end up in @newGrid at (4i, 4j) through (4i+3, 4j+3)
-        foreach my $blockRow (0..3) {
-          foreach my $blockCol (0..3) {
-            $newGrid[(4 * $i) + $blockRow][(4 * $j) + $blockCol] = $block[$blockRow][$blockCol];
-          }
-        }
-      }
-    }
+    @grid = ExpandThreeByThree(\@grid, \%patterns);
   }
 
-  @grid = @newGrid;
   ++$iterations;
+  my $pixelCount = CountPixels(\@grid);
+  print "After $iterations iterations, there are $pixelCount pixels on.\n";
 } while ($iterations < $TOTAL_ITERATIONS);
-
-my $pixelCount = CountPixels(\@grid);
-print "After $TOTAL_ITERATIONS iterations, there are $pixelCount pixels on.\n";
