7acf06a0036d3e142309ddd423e6a6f0f0cb2085
Ganon11 <stark3@gmail.com>
2018-12-07 10:48:58 -0500

    Day 7! Compiler optimization really saves me here.

---
+++ 2018/AdventOfCode/Day7/Day7.cpp
@@ -0,0 +1,198 @@
+// Day7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "Task.h"
+#include "Worker.h"
+
+bool get_tasks(vector<Task>& tasks);
+Task find_next_task(const vector<Task>& tasks, const set<wchar_t> completed_tasks);
+vector<Task> sort_tasks(const vector<Task>& tasks);
+void print_tasks_in_order(const vector<Task>& tasks);
+unsigned int perform_tasks(const vector<Task>& tasks_in_order);
+
+int main()
+{
+   vector<Task> tasks;
+   if (!get_tasks(tasks))
+   {
+      wcerr << L"Couldn't parse input." << endl;
+      return -1;
+   }
+
+   vector<Task> tasks_in_order{ sort_tasks(tasks) };
+   print_tasks_in_order(tasks_in_order);
+
+   unsigned int duration{ perform_tasks(tasks_in_order) };
+   wcout << L"All tasks complete in " << duration << L" seconds." << endl;
+
+   return 0;
+}
+
+bool get_tasks(vector<Task>& tasks)
+{
+   const wchar_t* FILENAME{ (USE_SAMPLE == 1) ? L"Sample.txt" : L"Input.txt" };
+   wifstream input{ FILENAME };
+   if (!input.good())
+   {
+      return false;
+   }
+
+   wregex task_regex{ L"Step (\\w) must be finished before step (\\w) can begin." };
+   wsmatch matches;
+   wstring line;
+   do
+   {
+      getline(input, line);
+
+      std::regex_search(line, matches, task_regex);
+      if (matches.size() != 3) // Whole match, dependency and task
+      {
+         wcerr << L"Error parsing line \"" << line << L"\"" << endl;
+         return false;
+      }
+
+      wchar_t task_name = matches[2].str()[0];
+      wchar_t dependency = matches[1].str()[0];
+
+      // Add indicated task
+      vector<Task>::iterator itr{ find(tasks.begin(), tasks.end(), task_name) };
+      if (tasks.end() == itr)
+      {
+         Task t{ task_name };
+         t.add_dependency(dependency);
+         tasks.push_back(t);
+      }
+      else
+      {
+         itr->add_dependency(dependency);
+      }
+
+      // Add indicated dependency
+      itr = find(tasks.begin(), tasks.end(), dependency);
+      if (tasks.end() == itr)
+      {
+         Task t{ dependency };
+         tasks.push_back(t);
+      }
+   } while (input.good());
+
+   sort(tasks.begin(), tasks.end());
+
+   return true;
+}
+
+Task find_next_task(const vector<Task>& tasks, const set<wchar_t> completed_tasks)
+{
+   for (const Task& t : tasks)
+   {
+      if (completed_tasks.end() != completed_tasks.find(t.get_name()))
+      {
+         continue; // Already done this task.
+      }
+
+      if (t.is_available(completed_tasks))
+      {
+         return t;
+      }
+   }
+
+   return Task{ numeric_limits<wchar_t>::max() };
+}
+
+vector<Task> sort_tasks(const vector<Task>& tasks)
+{
+   vector<Task> tasks_in_order;
+   tasks_in_order.reserve(tasks.size());
+   set<wchar_t> completed_tasks;
+   while (completed_tasks.size() != tasks.size())
+   {
+      Task next_task{ find_next_task(tasks, completed_tasks) };
+      tasks_in_order.push_back(next_task);
+      completed_tasks.insert(next_task.get_name());
+   }
+
+   return tasks_in_order;
+}
+
+void print_tasks_in_order(const vector<Task>& tasks)
+{
+   wcout << L"Tasks in order: ";
+   for (const Task& t : tasks)
+   {
+      wcout << t.get_name();
+   }
+   wcout << endl;
+}
+
+unsigned int perform_tasks(const vector<Task>& tasks_in_order)
+{
+   const size_t NUM_WORKERS{ (USE_SAMPLE == 1) ? 2 : 5 };
+
+   vector<Worker> workers;
+   workers.reserve(NUM_WORKERS);
+   for (size_t index = 0; index < NUM_WORKERS; ++index)
+   {
+      workers.push_back(Worker{});
+   }
+
+   unsigned int timer{ 0 };
+   set<wchar_t> completed_tasks;
+   set<wchar_t> assigned_tasks;
+   while (completed_tasks.size() != tasks_in_order.size())
+   {
+      // Check if any assigned tasks can be completed
+      for (Worker& w : workers)
+      {
+         if (w.is_currently_assigned_task() && w.has_completed_task(timer))
+         {
+            completed_tasks.insert(w.get_current_task());
+            w.deassign_task();
+         }
+      }
+
+      // Assign available tasks to available workers
+      for (const Task& task : tasks_in_order)
+      {
+         if (completed_tasks.end() != completed_tasks.find(task.get_name()))
+         {
+            continue; // Already done this task.
+         }
+
+         if (assigned_tasks.end() != assigned_tasks.find(task.get_name()))
+         {
+            continue; // Already being worked on
+         }
+
+         if (task.is_available(completed_tasks))
+         {
+            for (Worker& w : workers)
+            {
+               if (!w.is_currently_assigned_task())
+               {
+                  w.assign_task(task, timer);
+                  assigned_tasks.insert(task.get_name());
+                  break;
+               }
+            }
+         }
+      }
+
+      if (completed_tasks.size() != tasks_in_order.size())
+      {
+         ++timer;
+      }
+   }
+
+   return timer;
+}
---
+++ 2018/AdventOfCode/Day7/Task.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+#include "Task.h"
+
+Task::Task(const wchar_t name) : m_task_name{ name }
+{ }
+
+wchar_t Task::get_name() const
+{
+   return m_task_name;
+}
+
+void Task::add_dependency(const wchar_t dependency)
+{
+   m_dependencies.insert(dependency);
+}
+
+const std::set<wchar_t>& Task::get_dependencies() const
+{
+   return m_dependencies;
+}
+
+bool Task::operator==(const Task& other) const
+{
+   return m_task_name == other.get_name();
+}
+
+bool Task::operator<(const Task& other) const
+{
+   return m_task_name < other.get_name();
+}
+
+unsigned int Task::task_length() const
+{
+   unsigned int length{ static_cast<unsigned int>(m_task_name - L'A' + 1) };
+   if (1 != USE_SAMPLE)
+   {
+      length += 60;
+   }
+
+   return length;
+}
+
+bool Task::is_available(const std::set<wchar_t>& completed_tasks) const
+{
+   if (0 == m_dependencies.size()) // No dependencies
+   {
+      return true;
+   }
+
+   for (const wchar_t dependency : m_dependencies)
+   {
+      if (completed_tasks.end() == completed_tasks.find(dependency))
+      {
+         return false;
+         break;
+      }
+   }
+
+   return true;
+}
