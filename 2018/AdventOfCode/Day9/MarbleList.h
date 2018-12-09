173cb8be15f26bb9896284d180d1c9c1f7946770
Michael Stark <stark3@gmail.com>
2018-12-09 13:45:52 -0500

    Day 9! Pretty simple with my own doubly-linked list impl.

---
+++ 2018/AdventOfCode/Day9/MarbleList.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <map>
+#include <memory>
+
+struct MarbleListNode;
+
+typedef std::shared_ptr<MarbleListNode> MarbleListNodePtr;
+
+struct MarbleListNode
+{
+   MarbleListNode(const unsigned int points);
+
+   unsigned int m_points;
+   MarbleListNodePtr next;
+   MarbleListNodePtr previous;
+};
+
+class MarbleList
+{
+public:
+   MarbleList(const unsigned int players, const unsigned int max_marble);
+
+   unsigned int play_game();
+
+private:
+   MarbleListNodePtr add_marble(MarbleListNodePtr& left, const unsigned int marble);
+   unsigned int remove_marble(MarbleListNodePtr& marble);
+
+   unsigned int m_players;
+   unsigned int m_max_marble;
+   MarbleListNodePtr m_current;
+   std::map<unsigned int, unsigned int> m_player_scores;
+};
