0c20e20a1cee3ea0a20317ae9b0504b524ed8287
Mike Stark <mstark@factset.com>
2018-12-10 10:12:25 -0500

    Day 10 solution. Glad my heuristic worked out.

---
+++ 2018/AdventOfCode/Day10/Star.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+typedef std::pair<int, int> Position;
+typedef std::pair<int, int> Velocity;
+
+class Star
+{
+public:
+   Star(const std::wstring& line);
+
+   void move();
+   Position get_position() const;
+
+private:
+   Position m_position;
+   Velocity m_velocity;
+};
+
