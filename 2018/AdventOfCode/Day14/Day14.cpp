0f501deb8ed995ae1c70f77eff2e63decaeb1b10
Mike Stark <mstark@factset.com>
2018-12-14 16:21:08 -0500

    A bit of optimization. The biggest time sink by far now is vector insertion. I'm not sure I can do better?

--- 2018/AdventOfCode/Day14/Day14.cpp
+++ 2018/AdventOfCode/Day14/Day14.cpp
@@ -11,9 +11,9 @@
 
 typedef std::vector<unsigned short> DigitVector;
 
-DigitVector get_digits(unsigned short num);
+void get_digits(unsigned short num, DigitVector& digits);
 DigitVector get_digits(const std::wstring& input);
-bool generate_new_recipes(DigitVector& recipe_scores, size_t& elf_1, size_t& elf2);
+void generate_new_recipes(DigitVector& recipe_scores, size_t& elf_1, size_t& elf2);
 
 void part_1(const std::wstring& input);
 
@@ -34,19 +34,20 @@ int wmain(int argc, wchar_t* argv[]) {
   return 0;
 }
 
-DigitVector get_digits(unsigned short num) {
-  DigitVector digits;
-  if (0u == num) {
-    digits.push_back(0);
-  } else {
-    unsigned short num_digits{ static_cast<unsigned short>(log10(num) + 1) };
-    digits.reserve(num_digits);
-    for (int digit = 0; digit < num_digits; ++digit) {
-      digits.insert(digits.begin(), num % 10);
-      num /= 10;
-    }
+void get_digits(unsigned short num, DigitVector& digits) {
+  if (19 <= num) {
+    std::wcerr << L"Unexpected situation - sum is greater than 18" << std::endl;
+    return;
   }
-  return digits;
+
+  if (10 <= num) {
+    digits.reserve(2);
+    digits.push_back(1);
+    num -= 10;
+  }
+
+  digits.push_back(num);
+  return;
 }
 
 DigitVector get_digits(const std::wstring& input) {
@@ -59,18 +60,12 @@ DigitVector get_digits(const std::wstring& input) {
   return digits;
 }
 
-bool generate_new_recipes(DigitVector& recipe_scores, size_t& elf_1, size_t& elf_2) {
-  DigitVector digits{ get_digits(recipe_scores[elf_1] + recipe_scores[elf_2]) };
-  if (digits.size() == 0 || digits.size() > 2) {
-    std::wcerr << L"Unexpected result from get_digits" << std::endl;
-    return false;
-  }
-
+void generate_new_recipes(DigitVector& recipe_scores, size_t& elf_1, size_t& elf_2) {
+  DigitVector digits;
+  get_digits(recipe_scores[elf_1] + recipe_scores[elf_2], digits);
   recipe_scores.insert(recipe_scores.end(), digits.begin(), digits.end());
   elf_1 = (elf_1 + recipe_scores[elf_1] + 1) % recipe_scores.size();
   elf_2 = (elf_2 + recipe_scores[elf_2] + 1) % recipe_scores.size();
-
-  return true;
 }
 
 void part_1(const std::wstring& input) {
@@ -81,9 +76,7 @@ void part_1(const std::wstring& input) {
   size_t elf_1{ 0 }, elf_2{ 1 };
 
   while (recipe_scores.size() < RECIPES_TO_CREATE) {
-    if (!generate_new_recipes(recipe_scores, elf_1, elf_2)) {
-      return;
-    }
+    generate_new_recipes(recipe_scores, elf_1, elf_2);
   }
 
   // Part 1
@@ -104,10 +97,19 @@ size_t find_digits_in_recipes(const DigitVector& digits, const DigitVector& reci
     return NOT_FOUND;
   }
 
-  // Digits are either right at the end (if) or one index before the end (else if)
+  // Check if digits are at the end
   if (std::equal(digits.begin(), digits.end(), recipes.end() - digits.size())) {
     return recipes.size() - digits.size();
-  } else if (std::equal(digits.begin(), digits.end(), recipes.end() - digits.size() - 1)) {
+  }
+
+  // If there are the same number of digits as there are recipes, and we haven't found it yet, it's
+  // not there
+  if (digits.size() == recipes.size()) {
+    return NOT_FOUND;
+  }
+  
+  // Check if digits are at the end, offset by 1
+  if (std::equal(digits.begin(), digits.end(), recipes.end() - digits.size() - 1)) {
     return recipes.size() - digits.size() - 1;
   }
 
@@ -115,14 +117,12 @@ size_t find_digits_in_recipes(const DigitVector& digits, const DigitVector& reci
 }
 
 void part_2(const std::wstring& input) {
-  DigitVector input_digits{ get_digits(input) };
+  const DigitVector input_digits{ get_digits(input) };
   DigitVector recipe_scores{ 3, 7 };
   size_t elf_1{ 0 }, elf_2{ 1 };
   size_t found{ find_digits_in_recipes(input_digits, recipe_scores) };
   while (found == NOT_FOUND) {
-    if (!generate_new_recipes(recipe_scores, elf_1, elf_2)) {
-      return;
-    }
+    generate_new_recipes(recipe_scores, elf_1, elf_2);
     found = find_digits_in_recipes(input_digits, recipe_scores);
   }
 
