18fc8c22150cbf340a874224e924d00a59be1677
Michael Stark <stark3@gmail.com>
2018-12-14 22:45:03 -0500

    Added silly BATCH files for easily running binaries.

---
+++ 2018/AdventOfCode/RunAllSolutions.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+FOR /l %%d IN (1, 1, 25) DO (
+  SET _dir=Day%%d
+  IF EXIST !_dir! (
+    IF %%d EQU 14 (
+      SET _command_to_run=Day%%d\Win32\Release\Day%%d.exe
+    ) ELSE (
+      SET _command_to_run=Day%%d\Win32\Release\Day%%d.exe -filename Day%%d\Input.txt
+    )
+    !_command_to_run!
+  )
+)
+
+ENDLOCAL
