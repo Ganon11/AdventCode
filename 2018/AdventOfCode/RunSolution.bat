6e01bc52422d2bc51f664faaf764cf3e64ef3a9e
Ganon11 <stark3@gmail.com>
2018-12-14 22:45:03 -0500

    Added silly BATCH files for easily running binaries.

---
+++ 2018/AdventOfCode/RunSolution.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SETLOCAL
+IF %1 EQU 14 (
+  SET command=Day%1\Win32\Release\Day%1.exe
+) ELSE (
+  SET command=Day%1\Win32\Release\Day%1.exe -filename Day%1\Input.txt
+)
+ECHO %command%
+REM %command%
+REM PAUSE
