45c21aee4910e03d71b19a0e08c377b6c777ff8d
Ganon11 <stark3@gmail.com>
2018-12-21 16:50:16 -0500

    Day 14. Using classes and argparse.

--- 2015/Day14/src/Day14.py
+++ 2015/Day14/src/Day14.py
@@ -1,9 +1,87 @@
-import sys
+import argparse
+import re
+
+class Reindeer:
+  _REINDEER_PATTERN = re.compile('(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.', re.IGNORECASE)
+  def __init__(self, line):
+    match = Reindeer._REINDEER_PATTERN.match(line)
+    self.name = match.group(1)
+    self.speed = int(match.group(2))
+    self.run_duration = int(match.group(3))
+    self.rest_duration = int(match.group(4))
+    self.state = 'run'
+    self.timestamp = 0
+    self.distance = 0
+    self.points = 0
+
+  def print(self):
+    print("%s has traveled %d km. State: %s (since time %d). Score %d" % (self.name, self.distance, self.state, self.timestamp, self.points))
+
+  def step(self, time):
+    if self.state == 'run' and self.run_duration == (time - self.timestamp):
+      self.state = 'rest'
+      self.timestamp = time
+    elif self.state == 'rest' and self.rest_duration == (time - self.timestamp):
+      self.state = 'run'
+      self.timestamp = time
+
+    if self.state == 'run':
+      self.distance = self.distance + self.speed
+
+def get_reindeer(filename):
+  lines = None
+  with open(filename, 'r') as fp:
+    lines = fp.readlines()
+
+  reindeer = list()
+  for line in lines:
+    reindeer.append(Reindeer(line.rstrip()))
+
+  return reindeer
+
+def part_1(filename, steps):
+  all_reindeer = get_reindeer(filename)
+  for t in range(steps):
+    for r in all_reindeer:
+      r.step(t)
+
+  all_reindeer.sort(key=lambda x: x.distance, reverse=True)
+  all_reindeer[0].print()
+
+def part_2(filename, steps):
+  all_reindeer = get_reindeer(filename)
+  for t in range(steps):
+    for r in all_reindeer:
+      r.step(t)
+
+    all_reindeer.sort(key=lambda x: x.distance, reverse=True)
+    lead = all_reindeer[0].distance
+    for r in all_reindeer:
+      if lead == r.distance:
+        #print("Awarding point to %s" % r.name)
+        r.points = r.points + 1
+      else:
+        break
+
+  all_reindeer.sort(key=lambda x: x.points, reverse=True)
+  all_reindeer[0].print()
 
 def main():
-  filename = 'Sample.txt'
-  if len(sys.argv) > 1:
-    filename = sys.argv[1]
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--filename')
+  parser.add_argument('-s', '--steps')
+  args = parser.parse_args()
+
+  filename = '../input/Sample.txt'
+  if args.filename:
+    filename = args.filename
+
+  steps = 2503
+  if args.steps:
+    steps = int(args.steps)
+
+  part_1(filename, steps)
+  part_2(filename, steps)
 
 if __name__ == "__main__":
   main()
