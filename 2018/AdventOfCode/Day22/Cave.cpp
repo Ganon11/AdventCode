896ce244b36e3fef9a337304b464851a631299be
Ganon11 <stark3@gmail.com>
2018-12-23 00:04:45 -0500

    I guess I had this right for a while without knowing it!?

--- 2018/AdventOfCode/Day22/Cave.cpp
+++ 2018/AdventOfCode/Day22/Cave.cpp
@@ -1,12 +1,14 @@
 #include "pch.h"
 #include "Cave.h"
+#include <algorithm>
 #include <functional>
+#include <map>
 #include <queue>
 #include <set>
 #include <vector>
 
 namespace {
-unsigned long long EXTRA_SPACE{ 100 };
+unsigned long long EXTRA_SPACE{ 1000 };
 }
 
 Cave::Cave(const unsigned long long cave_depth, const advent_of_code::Position& target)
@@ -60,152 +62,180 @@ unsigned long long Cave::get_risk_level() const {
 namespace {
 const auto IMPOSSIBLE_TO_REACH{ std::numeric_limits<unsigned long long>::max() };
 
-void get_next_cost(const RegionType current_type,
+unsigned long long get_next_cost(const RegionType current_type,
   const RegionType next_type,
   const Equipment current_equipment,
-  const unsigned long long cost_so_far,
-  unsigned long long& new_cost,
-  Equipment& new_equipment) {
+  const unsigned long long cost_so_far) {
 
   if (current_type == next_type) {
-    new_cost = cost_so_far + 1;
-    new_equipment = current_equipment;
-    return;
+    return cost_so_far + 1;
   }
 
   switch (current_type) {
   case ROCKY:
-    if (next_type == NARROW) {
-      if (current_equipment == TORCH) {
-        new_cost = cost_so_far + 1;
-        new_equipment = current_equipment;
-        return;
-      } else if (current_equipment == CLIMBING_GEAR) {
-        new_cost = cost_so_far + 7;
-        new_equipment = TORCH;
-        return;
+    switch (next_type) {
+    case WET:
+      switch (current_equipment) {
+      case CLIMBING_GEAR:
+        return cost_so_far + 1;
       }
-    } else if (next_type == WET) {
-      if (current_equipment == TORCH) {
-        new_cost = cost_so_far + 7;
-        new_equipment = CLIMBING_GEAR;
-        return;
-      } else if (current_equipment == CLIMBING_GEAR) {
-        new_cost = cost_so_far + 1;
-        new_equipment = current_equipment;
-        return;
+      break;
+    case NARROW:
+      switch (current_equipment) {
+      case TORCH:
+        return cost_so_far + 1;
       }
+      break;
     }
     break;
   case WET:
-    if (next_type == NARROW) {
-      if (current_equipment == CLIMBING_GEAR) {
-        new_cost = cost_so_far + 7;
-        new_equipment = NEITHER;
-        return;
-      } else if (current_equipment == NEITHER) {
-        new_cost = cost_so_far + 1;
-        new_equipment = current_equipment;
-        return;
+    switch (next_type) {
+    case ROCKY:
+      switch (current_equipment) {
+      case CLIMBING_GEAR:
+        return cost_so_far + 1;
       }
-    } else if (next_type == ROCKY) {
-      if (current_equipment == CLIMBING_GEAR) {
-        new_cost = cost_so_far + 1;
-        new_equipment = current_equipment;
-        return;
-      } else if (current_equipment == NEITHER) {
-        new_cost = cost_so_far + 7;
-        new_equipment = CLIMBING_GEAR;
-        return;
+      break;
+    case NARROW:
+      switch (current_equipment) {
+      case NEITHER:
+        return cost_so_far + 1;
       }
+      break;
     }
     break;
   case NARROW:
-    if (next_type == WET) {
-      if (current_equipment == TORCH) {
-        new_cost = cost_so_far + 7;
-        new_equipment = NEITHER;
-        return;
-      } else if (current_equipment == NEITHER) {
-        new_cost = cost_so_far + 1;
-        new_equipment = current_equipment;
-        return;
+    switch (next_type) {
+    case ROCKY:
+      switch (current_equipment) {
+      case TORCH:
+        return cost_so_far + 1;
       }
-    } else if (next_type == ROCKY) {
-      if (current_equipment == TORCH) {
-        new_cost = cost_so_far + 1;
-        new_equipment = current_equipment;
-        return;
-      } else if (current_equipment == NEITHER) {
-        new_cost = cost_so_far + 7;
-        new_equipment = TORCH;
-        return;
+      break;
+    case WET:
+      switch (current_equipment) {
+      case NEITHER:
+        return cost_so_far + 1;
       }
+      break;
     }
     break;
   }
 
-  new_cost = ::IMPOSSIBLE_TO_REACH;
-  return;
+  return ::IMPOSSIBLE_TO_REACH;
 }
 }
 
 unsigned long long Cave::cost_to_target() const {
-  auto cost_heuristic{ [](const Explorer& e1, const Explorer& e2) {
-    return e1.get_cost_so_far() > e2.get_cost_so_far();
-  } };
+  std::map<Explorer, unsigned long long> cost_so_far;
+  advent_of_code::Position target{ m_target };
 
+  auto cost_heuristic{ [&cost_so_far, &target](const Explorer& e1, const Explorer& e2) {
+    unsigned long long cost1;
+    auto found1{ cost_so_far.find(e1) };
+    if (cost_so_far.end() == found1) {
+      cost1 = IMPOSSIBLE_TO_REACH;
+    } else {
+      cost1 = found1->second;
+    }
+
+    unsigned long long cost2;
+    auto found2{ cost_so_far.find(e2) };
+    if (cost_so_far.end() == found2) {
+      cost2 = IMPOSSIBLE_TO_REACH;
+    } else {
+      cost2 = found2->second;
+    }
+
+    if (cost1 > cost2) {
+      return true;
+    } else if (cost1 == cost2
+      && target.distance_to(e1.get_position()) > target.distance_to(e2.get_position())) {
+      return true;
+    }
+
+    return false;
+  } };
 
   std::priority_queue<Explorer,
     std::vector<Explorer>,
     std::function<bool(Explorer, Explorer)>> frontier{ cost_heuristic };
 
   frontier.push(Explorer{});
-  std::set<Explorer> seen;
-  seen.insert(Explorer{});
+  cost_so_far[Explorer{}] = 0;
 
   while (!frontier.empty()) {
     Explorer current{ frontier.top() };
     frontier.pop();
     advent_of_code::Position current_position{ current.get_position() };
+    Equipment current_equipment{ current.get_currently_equipped() };
+    unsigned long long current_cost{ cost_so_far.at(current) };
 
-    // Made it to target - remember to switch to the torch!
-    if (m_target == current_position) {
-      if (TORCH == current.get_currently_equipped()) {
-        return current.get_cost_so_far();
-      } else {
-        return current.get_cost_so_far() + 7;
-      }
+    // Made it to target with torch out!
+    if (m_target == current_position && TORCH == current_equipment) {
+      return current_cost;
     }
 
     // Still looking
     RegionType current_region{ m_cave.at(current_position) };
+
+    // Add equipment swaps
+    Explorer equipment_swap{ current_position, current_equipment };
+    switch (current_region) {
+    case ROCKY:
+      if (CLIMBING_GEAR == current_equipment) {
+        equipment_swap.set_equipment(TORCH);
+      } else {
+        equipment_swap.set_equipment(CLIMBING_GEAR);
+      }
+      break;
+    case WET:
+      if (CLIMBING_GEAR == current_equipment) {
+        equipment_swap.set_equipment(NEITHER);
+      } else {
+        equipment_swap.set_equipment(CLIMBING_GEAR);
+      }
+      break;
+    case NARROW:
+      if (TORCH == current_equipment) {
+        equipment_swap.set_equipment(NEITHER);
+      } else {
+        equipment_swap.set_equipment(TORCH);
+      }
+      break;
+    }
+
+    if (cost_so_far.end() == cost_so_far.find(equipment_swap)) {
+      cost_so_far[equipment_swap] = current_cost + 7;
+      frontier.push(equipment_swap);
+    }
+
     for (auto next : current_position.get_adjacent_positions()) {
       if (next.m_x < 0 || next.m_y < 0) {
         // Impossible to dig through cave at these points.
         continue;
       }
 
+      if (next.m_x > 1000 || next.m_y > 1000) {
+        // I've gone too far?
+        continue;
+      }
+
       RegionType next_region{ m_cave.at(next) };
+      unsigned long long new_cost{ ::get_next_cost(current_region, next_region, current_equipment, current_cost) };
 
-      Equipment next_equipment;
-      unsigned long long new_cost;
-      ::get_next_cost(current_region,
-        next_region,
-        current.get_currently_equipped(),
-        current.get_cost_so_far(),
-        new_cost,
-        next_equipment);
+      if (IMPOSSIBLE_TO_REACH == new_cost) {
+        // Can't get to next area with current equipment.
+        // Switching equipment is handled above.
+        continue;
+      }
 
-      Explorer new_state{ next, next_equipment, new_cost };
-      auto seen_already{ seen.find(new_state) };
-      if (seen.end() == seen.find(new_state)) {
-        seen.insert(new_state);
-        frontier.push(new_state);
-      } else if (new_cost < seen_already->get_cost_so_far()) {
-        seen.erase(seen_already);
-        seen.insert(new_state);
-        frontier.push(new_state);
+      Explorer new_explorer{ next, current_equipment };
+
+      auto seen_already{ cost_so_far.find(new_explorer) };
+      if (cost_so_far.end() == seen_already || new_cost < seen_already->second) {
+        cost_so_far[new_explorer] = new_cost;
+        frontier.push(new_explorer);
       }
     }
   }
--- 2018/AdventOfCode/Day22/Explorer.cpp
+++ 2018/AdventOfCode/Day22/Explorer.cpp
@@ -1,11 +1,21 @@
 #include "pch.h"
 #include "Explorer.h"
 
-Explorer::Explorer(const advent_of_code::Position& p, const Equipment equipped, const unsigned long long cost_so_far)
-  : m_position{ p },
-  m_currently_equipped{ equipped },
-  m_cost_so_far{ cost_so_far }
-{}
+Explorer::Explorer(const advent_of_code::Position& p, const Equipment equipped)
+  : m_currently_equipped{ equipped }
+{
+  m_position.m_x = p.m_x;
+  m_position.m_y = p.m_y;
+}
+
+void Explorer::set_position(const advent_of_code::Position& p) {
+  m_position.m_x = p.m_x;
+  m_position.m_y = p.m_y;
+}
+
+void Explorer::set_equipment(const Equipment e) {
+  m_currently_equipped = e;
+}
 
 advent_of_code::Position Explorer::get_position() const {
   return m_position;
@@ -15,14 +25,10 @@ Equipment Explorer::get_currently_equipped() const {
   return m_currently_equipped;
 }
 
-unsigned long long Explorer::get_cost_so_far() const {
-  return m_cost_so_far;
-}
-
 bool Explorer::operator<(const Explorer& other) const {
   if (m_position < other.m_position) {
     return true;
-  } else if (m_position == other.m_position && m_cost_so_far < other.m_cost_so_far) {
+  } else if (m_position == other.m_position && m_currently_equipped < other.m_currently_equipped) {
     return true;
   }
 
