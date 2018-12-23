58d90209b1123d1ff95bc841d2ad3463721d85eb
Michael Stark <stark3@gmail.com>
2018-12-23 00:04:45 -0500

    I guess I had this right for a while without knowing it!?

--- 2018/AdventOfCode/Day22/Explorer.h
+++ 2018/AdventOfCode/Day22/Explorer.h
@@ -12,13 +12,13 @@ enum Equipment
 class Explorer
 {
 public:
-  Explorer(const advent_of_code::Position& p = advent_of_code::ORIGIN,
-    const Equipment equipped = TORCH,
-    const unsigned long long cost_so_far = 0);
+  Explorer(const advent_of_code::Position& p = advent_of_code::ORIGIN, const Equipment equipped = TORCH);
+
+  void set_position(const advent_of_code::Position& p);
+  void set_equipment(const Equipment e);
 
   advent_of_code::Position get_position() const;
   Equipment get_currently_equipped() const;
-  unsigned long long get_cost_so_far() const;
 
   bool operator<(const Explorer& other) const;
   bool operator==(const Explorer& other) const;
@@ -26,5 +26,4 @@ public:
 private:
   advent_of_code::Position m_position;
   Equipment m_currently_equipped;
-  unsigned long long m_cost_so_far;
 };
