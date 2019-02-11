8a50b807424a6f0f61abd7c874cb67a1c6822264
Ganon11 <stark3@gmail.com>
2019-02-11 16:21:42 -0500

    Day 20

--- 2015/Day20/src/Day20.py
+++ 2015/Day20/src/Day20.py
@@ -1,34 +1,43 @@
 import argparse
 
+from functools import reduce
+
+def get_divisors(num):
+  return set(reduce(list.__add__, ([i, num // i] for i in range(1, int(num ** 0.5) + 1) if num % i == 0)))
+
 def sum_of_divisors(num):
-  return sum([i for i in range(1, num) if num % i == 0])
+  return sum(get_divisors(num))
+
+def limited_sum_of_divisors(num):
+  return sum(d for d in get_divisors(num) if num / d <= 50) * 11
 
 def part_1(target):
-  house = 0
-  factor = 100000
+  house = 1
   presents = sum_of_divisors(house) * 10
-  while True:
-    #print("Checking %d presents vs target %d" % (presents, target))
-    if presents < target:
-      #print("Adding")
-      house = house + factor
-    else:
-      print("Subtracting and reducing factor from %d to %d" % (factor, int(factor / 10)))
-      house = house - factor
-      factor = int(factor / 10)
-
+  while presents < target:
+    house = house + 1
     presents = sum_of_divisors(house) * 10
-    if factor == 1 and presents >= target:
-      break
+  print("House %d got %d presents" % (house, presents))
 
+def part_2(target):
+  house = 1
+  presents = limited_sum_of_divisors(house)
+  while presents < target:
+    house = house + 1
+    presents = limited_sum_of_divisors(house)
   print("House %d got %d presents" % (house, presents))
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-t', '--target', type=int, default=34000000)
+  parser.add_argument('-n', '--number', type=int)
   args = parser.parse_args()
 
-  part_1(args.target)
+  if args.number:
+    print(get_divisors(args.number))
+  else:
+    part_1(args.target)
+    part_2(args.target)
 
 if __name__ == "__main__":
   main()
