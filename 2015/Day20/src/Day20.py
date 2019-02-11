2a4852b1b7020435136429f75f7a4aa299fa730f
Mike Stark <mstark@factset.com>
2019-02-11 09:54:19 -0500

    Some code for 2015 day 20. Doesn't give the right answer.

--- 2015/Day20/src/Day20.py
+++ 2015/Day20/src/Day20.py
@@ -1,9 +1,34 @@
-import sys
+import argparse
+
+def sum_of_divisors(num):
+  return sum([i for i in range(1, num) if num % i == 0])
+
+def part_1(target):
+  house = 0
+  factor = 100000
+  presents = sum_of_divisors(house) * 10
+  while True:
+    #print("Checking %d presents vs target %d" % (presents, target))
+    if presents < target:
+      #print("Adding")
+      house = house + factor
+    else:
+      print("Subtracting and reducing factor from %d to %d" % (factor, int(factor / 10)))
+      house = house - factor
+      factor = int(factor / 10)
+
+    presents = sum_of_divisors(house) * 10
+    if factor == 1 and presents >= target:
+      break
+
+  print("House %d got %d presents" % (house, presents))
 
 def main():
-  filename = 'Sample.txt'
-  if len(sys.argv) > 1:
-    filename = sys.argv[1]
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-t', '--target', type=int, default=34000000)
+  args = parser.parse_args()
+
+  part_1(args.target)
 
 if __name__ == "__main__":
   main()
