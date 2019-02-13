ab68e86682792e460e2042b74d63a64da5158d0a
Ganon11 <stark3@gmail.com>
2019-02-12 19:14:27 -0500

    Day 24. Combinations are cool.

--- 2015/Day24/src/Day24.py
+++ 2015/Day24/src/Day24.py
@@ -1,9 +1,35 @@
+import argparse
+import functools
+import itertools
+import operator
 import sys
 
+def get_packages(filename):
+  packages = set()
+  with open(filename, 'r') as fp:
+    for line in fp.readlines():
+      packages.add(int(line.rstrip()))
+  return packages
+
+def divide_packages(packages, count):
+  target_weight = sum(packages) // count
+  print(target_weight)
+  for num_packages in range(len(packages)):
+    qes = [functools.reduce(operator.mul, c) for c in itertools.combinations(packages, num_packages) if sum(c) == target_weight]
+    if qes:
+      print("Minimum qes %d" % min(qes))
+      return
+
 def main():
-  filename = 'Sample.txt'
-  if len(sys.argv) > 1:
-    filename = sys.argv[1]
+  parser = argparse.ArgumentParser()
+  parser.add_argument('filename')
+  args = parser.parse_args()
+
+  packages = get_packages(args.filename)
+
+  print(packages)
+  divide_packages(packages, 3)
+  divide_packages(packages, 4)
 
 if __name__ == "__main__":
   main()
