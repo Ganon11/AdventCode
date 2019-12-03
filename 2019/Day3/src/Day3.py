1d9f89c2991b04e3bcefd0301737a4c259c70536
Ganon11 <stark3@gmail.com>
2019-12-03 11:00:04 -0500

    Creating a Position library. This has been helpful in previous years. I may copy more code from last year's C++ project.

--- 2019/Day3/src/Day3.py
+++ 2019/Day3/src/Day3.py
@@ -1,55 +1,6 @@
 import argparse
 import os
-
-class Position:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
-
-  def ManhattanDistance(self, other):
-    return abs(self.x - other.x) + abs(self.y - other.y)
-
-  def DistanceToOrigin(self):
-    return abs(self.x) + abs(self.y)
-
-  def __hash__(self):
-    return hash(str(self))
-
-  def __eq__(self, other):
-    return self.x == other.x and self.y == other.y
-
-  def __str__(self):
-    return f'{self.x}, {self.y}'
-
-def walk_path(moves):
-  p = Position(0, 0)
-  visited = list()
-  for m in moves:
-    direction = m[0]
-    count = int(m[1:])
-    if direction == 'U':
-      for i in range(0, count):
-        newP = Position(p.x, p.y + 1)
-        visited.append(newP)
-        p = newP
-    elif direction == 'D':
-      for i in range(0, count):
-        newP = Position(p.x, p.y - 1)
-        visited.append(newP)
-        p = newP
-    elif direction == 'L':
-      for i in range(0, count):
-        newP = Position(p.x - 1, p.y)
-        visited.append(newP)
-        p = newP
-    elif direction == 'R':
-      for i in range(0, count):
-        newP = Position(p.x + 1, p.y)
-        visited.append(newP)
-        p = newP
-    else:
-      print(f'Unknown direction {direction}')
-  return visited
+import Position
 
 def main():
   parser = argparse.ArgumentParser()
@@ -61,8 +12,8 @@ def main():
   with open(args.filename, 'r') as f:
     text = f.readlines()
 
-  path1 = walk_path(text[0].split(','))
-  path2 = walk_path(text[1].split(','))
+  path1 = Position.walk_path(text[0].split(','))
+  path2 = Position.walk_path(text[1].split(','))
 
   path1set = {p for p in path1 if p.x != 0 and p.y != 0}
   path2set = {p for p in path2 if p.x != 0 and p.y != 0}
