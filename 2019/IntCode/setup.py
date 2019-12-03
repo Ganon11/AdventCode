e4d7a1dcbd2641ffa11708b5eeac5fe55dcff498
Michael Stark <stark3@gmail.com>
2019-12-02 19:59:23 -0500

    Making IntCode into a real package.

---
+++ 2019/IntCode/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="IntCode-Ganon11",
+  version="0.0.10",
+  author="Ganon11",
+  author_email="stark3@gmail.com",
+  description="An interpreter for Advent of Code 2019's IntCode",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Ganon11/AdventCode",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6'
+)
