ce746aa478304869df6f272c6825fce56882e044
Ganon11 <stark3@gmail.com>
2019-12-06 10:41:13 -0500

    Tweaking code.

--- 2019/Day6/src/Day6.py
+++ 2019/Day6/src/Day6.py
@@ -13,24 +13,12 @@ class SpaceObject:
   def add_child(self, child):
     self.children.append(child)
 
-  def count_orbits(self):
-    if self.parent is None:
-      return 0
-
-    return 1 + self.parent.count_orbits()
-
   def get_children_set(self):
     children = set(map(lambda o: o.name, self.children))
     for child in self.children:
       children.update(child.get_children_set())
 
     return children
-
-  def get_orbit_chain(self):
-    if self.parent is None:
-      return [self.name]
-
-    return [self.name] + self.parent.get_orbit_chain()
 
   def distance_to(self, target):
     if target in map(lambda o: o.name, self.children):
@@ -50,34 +38,18 @@ class SpaceObject:
     return f'Object {self.name} has {len(self.children)} children'
 
 def part1(objects):
-  root = 'COM'
+  total = 0
+  root = objects['COM']
+  for child in root.get_children_set():
+    total += root.distance_to(child)
 
-  q = objects[root].children
-  seen = set([root])
-  distances = 0
-  level = 1
-  while len(q) > 0:
-    #print(f'doing level {level}, len(q): {len(q)}')
-    q_next = []
-    for node in q:
-      #print(node)
-      if node.name not in seen:
-        distances += level
-        neighbors = objects.get(node.name)
-        q_next.extend(neighbors.children)
-        seen.add(node.name)
-    level +=1
-    q = q_next
-  return distances
+  print(total)
 
 def part2(objects):
   (you, santa) = ('YOU', 'SAN')
   current = objects['COM']
   while True:
     currentChildren = current.get_children_set()
-    if you not in currentChildren or santa not in currentChildren:
-      raise Exception('you dun goofd')
-
     childFound = False
     for child in current.children:
       childSet = child.get_children_set()
@@ -85,23 +57,14 @@ def part2(objects):
         childFound = True
         current = child
         break
-
     if not childFound:
       break
 
-  print(f'lowest common ancestor is {current}')
-  #print(f'distance from ancestor to YOU is {current.distance_to(you)}')
-  #print(f'distance from ancestor to SAN is {current.distance_to(santa)}')
   print(current.distance_to(you) + current.distance_to(santa) - 2)
 
-def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-f', '--filename', default='../input/sample1.txt')
-  parser.add_argument('-p', '--part', choices=[1, 2], default=1, type=int)
-  args = parser.parse_args()
-
+def get_space_objects(filename):
   lines = None
-  with open(args.filename, 'r') as f:
+  with open(filename, 'r') as f:
     lines = f.readlines()
 
   objects = dict()
@@ -122,9 +85,29 @@ def main():
       childObject = SpaceObject(child, parentObject)
       objects[child] = childObject
 
-  if args.part == 1:
-    print(part1(objects))
+  return objects
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--use-sample', action='store_true')
+  parser.add_argument('-f', '--filename', default='../input/sample1.txt')
+  parser.add_argument('-p', '--part', choices=[1, 2], default=1, type=int)
+  args = parser.parse_args()
+
+  filename = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'input')
+  if args.use_sample:
+    filename = os.path.join(filename, 'sample.txt')
   else:
+    filename = os.path.join(filename, 'input.txt')
+
+  objects = get_space_objects(filename)
+
+  if args.part == 1:
+    part1(objects)
+  else:
+    if args.use_sample:
+      objects['YOU'] = SpaceObject('YOU', objects['K'])
+      objects['SAN'] = SpaceObject('SAN', objects['I'])
     part2(objects)
 
 if __name__ == "__main__":
