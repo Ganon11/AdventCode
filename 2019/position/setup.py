923998cc3b786322b404ae286483c73d3f5aa850
Michael Stark <stark3@gmail.com>
2019-12-06 20:42:19 -0500

    Fixing up setup.py files.

--- 2019/position/setup.py
+++ 2019/position/setup.py
@@ -5,18 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="position-Ganon11",
-  version="0.0.1",
+  version="1.0.0",
   author="Ganon11",
   author_email="stark3@gmail.com",
   description="A library for tracking positions on a 2D coordinate system",
   long_description=long_description,
-  long_description_content_type="text/markdown",
   url="https://github.com/Ganon11/AdventCode",
   packages=setuptools.find_packages(),
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
-  ],
-  python_requires='>=3.6'
+  ]
 )
