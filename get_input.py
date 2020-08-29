d28c73bc0a02ac243e56744fffd68a9e5fae351c
Ganon11 <stark3@gmail.com>
2020-08-29 11:57:56 -0400

    Further enhance get_input
    Now optionally accepts a path to destination file

--- get_input.py
+++ get_input.py
@@ -6,16 +6,22 @@ import argparse
 import os
 import requests
 
-def create_input_file(year, day):
+def create_input_file(year, day, path):
   """Fetches my input from Advent of Code."""
   daystr = 'Day' + str(day)
-  os.mkdir(os.path.join(str(year), daystr, 'input'))
   url = 'https://adventofcode.com/' + str(year) + '/day/' + str(day) + '/input'
+
   cookies = dict()
   with open('session.txt', 'r') as file:
     cookies['session'] = file.read()
 
-  path = os.path.join(str(year), daystr, 'input', 'input.txt')
+  if path is None:
+    path = os.path.join(str(year), daystr, 'input', 'input.txt')
+
+  dirname = os.path.dirname(path)
+  if dirname != '' and not os.path.exists(dirname):
+    os.mkdir(dirname)
+
   with open(path, 'w') as file:
     input_request = requests.get(url=url, cookies=cookies)
     file.write(input_request.text)
@@ -25,9 +31,10 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--day', '-d', type=int)
   parser.add_argument('--year', '-y', type=int)
+  parser.add_argument('--path', '-p', type=str)
   args = parser.parse_args()
 
-  create_input_file(args.year, args.day)
+  create_input_file(args.year, args.day, args.path)
 
 if __name__ == "__main__":
   main()
