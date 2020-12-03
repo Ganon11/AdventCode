a54574f28e8403c2c328d338572fd1c415c8c9a5
Ganon11 <stark3@gmail.com>
2020-12-03 18:31:21 -0500

    Day 3 in ruby!

---
+++ 2020/Day3/src/Day3.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--filename', default='../input/sample1.txt')
+  parser.add_argument('-p', '--part', choices=[1, 2], default=1, type=int)
+  args = parser.parse_args()
+
+if __name__ == "__main__":
+  main()
