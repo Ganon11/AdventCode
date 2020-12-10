3110607bedc420fdab662fa296adf90ac77319f3
Ganon11 <stark3@gmail.com>
2020-12-10 12:39:20 -0500

    Day 10 solution! Initially coded a graph based solution before using Dynamic Programming.

---
+++ 2020/Day10/src/Day10.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby
+
+require 'optparse'
+
+def part1(adapters)
+  target = adapters[-1] + 3
+  diffs = {
+    1=>0,
+    2=>0,
+    3=>1 # The last jump to our device is always 3
+  }
+  adapters.each_with_index do |value, index|
+    if index == 0
+      diffs[value] += 1
+      next
+    end
+
+    diff = value - adapters[index - 1]
+    if diff <= 0 or 3 < diff
+      puts "Invalid adapter change at #{index}"
+      break
+    end
+
+    diffs[diff] += 1
+  end
+
+  return diffs[1] * diffs[3]
+end
+
+def part2(adapters)
+  target = adapters[-1] + 3
+  adapters.push(target)
+  dynamic = {0 => 1}
+  adapters.each_with_index do |adapter, index|
+    sum = 0
+    if dynamic.key?(adapter - 1)
+      sum += dynamic[adapter - 1]
+    end
+
+    if dynamic.key?(adapter - 2)
+      sum += dynamic[adapter - 2]
+    end
+
+    if dynamic.key?(adapter - 3)
+      sum += dynamic[adapter - 3]
+    end
+
+    dynamic[adapter] = sum
+  end
+
+  return dynamic[target]
+end
+
+options = {
+  :part => 1,
+  :filename => "../input/sample1.txt"
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: {daystr}.rb [options]"
+  opts.on("-p PART", "--part=PART", Integer, "Part 1 or 2")
+  opts.on("-f FILENAME", "--filename=FILENAME", String, "Which filename to use?")
+end.parse!(into: options)
+
+adapters = IO.readlines(options[:filename]).map{|line| line.strip.to_i}.sort
+
+if options[:part] == 1
+  puts part1(adapters)
+elsif options[:part] == 2
+  puts part2(adapters)
+end
