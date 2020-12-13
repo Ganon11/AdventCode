a0f1c8e637a334643afb0522a5c8d0d1f44940d6
Ganon11 <stark3@gmail.com>
2020-12-12 19:31:47 -0500

    Lots of cleanup!

--- 2020/Day12/src/Day12.rb
+++ 2020/Day12/src/Day12.rb
@@ -1,208 +1,62 @@
 #!/usr/bin/ruby
 
 class Ferry
-  @@delta_x = {
-    :NORTH => 0,
-    :SOUTH => 0,
-    :EAST => 1,
-    :WEST => -1
-  }
-
-  @@delta_y = {
-    :NORTH => 1,
-    :SOUTH => -1,
-    :EAST => 0,
-    :WEST => 0
-  }
-
   def initialize()
-    @x = 0
-    @y = 0
-    @facing = :EAST
+    @position = { :x => 0, :y => 0 }
+    # The first value is used as the delta X, and the second value is used as the delta Y, for
+    # determining what changes when we move forward.
+    # When we rotate left or right, we rotate the @direction array accordingly to update which
+    # way we're facing.
+    # Idea adapted from a reddit comment!
+    @direction = [1, 0, -1, 0]
   end
 
   def follow_instruction(instruction)
-    action = instruction.chars[0]
-    instruction[0] = ''
-    value = instruction.to_i
-    case action
-    when 'N'
-      @y += value
-    when 'S'
-      @y -= value
-    when 'E'
-      @x += value
-    when 'W'
-      @x -= value
-    when 'L'
-      rotate(:COUNTERCLOCKWISE, value)
-    when 'R'
-      rotate(:CLOCKWISE, value)
+    value = instruction[1..].to_i
+    case instruction[0]
+    when 'N' then @position[:y] += value
+    when 'S' then @position[:y] -= value
+    when 'E' then @position[:x] += value
+    when 'W' then @position[:x] -= value
+    when 'L' then @direction.rotate!(-1 * (value / 90))
+    when 'R' then @direction.rotate!(value / 90)
     when 'F'
-      case @facing
-      when :NORTH
-        @y += value
-      when :SOUTH
-        @y -= value
-      when :EAST
-        @x += value
-      when :WEST
-        @x -= value
-      end
-    end
-  end
-
-  def rotate(direction, degrees)
-    case degrees
-    when 90
-      case direction
-      when :CLOCKWISE
-        case @facing
-        when :NORTH
-          @facing = :EAST
-        when :SOUTH
-          @facing = :WEST
-        when :EAST
-          @facing = :SOUTH
-        when :WEST
-          @facing = :NORTH
-        end
-      when :COUNTERCLOCKWISE
-        case @facing
-        when :NORTH
-          @facing = :WEST
-        when :SOUTH
-          @facing = :EAST
-        when :EAST
-          @facing = :NORTH
-        when :WEST
-          @facing = :SOUTH
-        end
-      else
-        puts "Unrecognized direction"
-      end
-    when 180
-      # Clockwise v counter-clockwise doesn't matter
-      case @facing
-      when :NORTH
-        @facing = :SOUTH
-      when :SOUTH
-        @facing = :NORTH
-      when :EAST
-        @facing = :WEST
-      when :WEST
-        @facing = :EAST
-      end
-    when 270
-      case direction
-      when :CLOCKWISE
-        case @facing
-        when :NORTH
-          @facing = :WEST
-        when :SOUTH
-          @facing = :EAST
-        when :EAST
-          @facing = :NORTH
-        when :WEST
-          @facing = :SOUTH
-        end
-      when :COUNTERCLOCKWISE
-        case @facing
-        when :NORTH
-          @facing = :EAST
-        when :SOUTH
-          @facing = :WEST
-        when :EAST
-          @facing = :SOUTH
-        when :WEST
-          @facing = :NORTH
-        end
-      else
-        puts "Unrecognized direction"
-      end
-    else
-      puts "Unrecognized degrees"
+      @position[:x] += (@direction[0] * value)
+      @position[:y] += (@direction[1] * value)
     end
   end
 
   def distance_from_origin
-    @x.abs + @y.abs
+    @position[:x].abs + @position[:y].abs
   end
 end
 
 class FerryWithWaypoint < Ferry
   def initialize
     super
-    @waypoint_x = 10
-    @waypoint_y = 1
+    @waypoint = { :x => 10, :y => 1 }
   end
 
   def follow_instruction(instruction)
-    action = instruction.chars[0]
-    instruction[0] = ''
-    value = instruction.to_i
-    case action
-    when 'N'
-      @waypoint_y += value
-    when 'S'
-      @waypoint_y -= value
-    when 'E'
-      @waypoint_x += value
-    when 'W'
-      @waypoint_x -= value
+    value = instruction[1..].to_i
+    case instruction[0]
+    when 'N' then @waypoint[:y] += value
+    when 'S' then @waypoint[:y] -= value
+    when 'E' then @waypoint[:x] += value
+    when 'W' then @waypoint[:x] -= value
     when 'L'
-      case value
-      when 90
-        tmp = @waypoint_y
-        @waypoint_y = @waypoint_x
-        @waypoint_x = -tmp
-      when 180
-        @waypoint_x *= -1
-        @waypoint_y *= -1
-      when 270
-        tmp = @waypoint_y
-        @waypoint_y = -@waypoint_x
-        @waypoint_x = tmp
+      (value / 90).times do
+        @waypoint = { :x => -@waypoint[:y], :y => @waypoint[:x] }
       end
     when 'R'
-      case value
-      when 90
-        tmp = @waypoint_y
-        @waypoint_y = -@waypoint_x
-        @waypoint_x = tmp
-      when 180
-        @waypoint_x *= -1
-        @waypoint_y *= -1
-      when 270
-        tmp = @waypoint_y
-        @waypoint_y = @waypoint_x
-        @waypoint_x = -tmp
+      (value / 90).times do
+        @waypoint = { :x => @waypoint[:y], :y => -@waypoint[:x] }
       end
     when 'F'
-      value.times do |i|
-        @x += @waypoint_x
-        @y += @waypoint_y
-      end
+      @position[:x] += (@waypoint[:x] * value)
+      @position[:y] += (@waypoint[:y] * value)
     end
   end
-end
-
-def part1(lines)
-  ferry = Ferry.new
-  lines.each do |line|
-    ferry.follow_instruction(line)
-  end
-
-  return ferry.distance_from_origin
-end
-
-def part2(lines)
-  ferry = FerryWithWaypoint.new
-  lines.each do |line|
-    ferry.follow_instruction(line)
-  end
-
-  return ferry.distance_from_origin
 end
 
 require 'optparse'
@@ -220,8 +74,15 @@ end.parse!(into: options)
 
 instructions = IO.readlines(options[:filename]).map {|line| line.strip}
 
+ferry = nil
 if options[:part] == 1
-  puts part1(instructions)
+  ferry = Ferry.new
 elsif options[:part] == 2
-  puts part2(instructions)
+  ferry = FerryWithWaypoint.new
 end
+
+instructions.each do |instruction|
+  ferry.follow_instruction(instruction)
+end
+
+puts ferry.distance_from_origin
