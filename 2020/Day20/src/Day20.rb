96f78ab7bcc58aee21ac0e5062d833c6ac77d406
Ganon11 <stark3@gmail.com>
2020-12-22 16:47:50 -0500

    I can now get the tile IDs in the right position, but I am unsure how to flip/rotate them appropriately.

--- 2020/Day20/src/Day20.rb
+++ 2020/Day20/src/Day20.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'optparse'
+require 'set'
 
 class Tile
   attr_reader :id
@@ -47,32 +48,10 @@ class Tile
   end
 end
 
-def part1(tiles)
-  edge_to_tiles = {}
-  tiles.each do |tile|
-    tile.edges.each do |edge|
-      edge_str = edge.join("")
-      if edge_to_tiles.key?(edge_str)
-        edge_to_tiles[edge_str].push(tile.id)
-      else
-        edge_to_tiles[edge_str] = [tile.id]
-      end
-    end
+def print_puzzle_ids(puzzle)
+  puzzle.each do |row|
+    puts row.map{|tile| tile.nil? ? "?" : tile.id}.join(", ")
   end
-
-  tile_unique_edge_count = {}
-  edge_to_tiles.keys.each do |edge|
-    if edge_to_tiles[edge].size == 1
-      tile_id = edge_to_tiles[edge][0]
-      if tile_unique_edge_count.key?(tile_id)
-        tile_unique_edge_count[tile_id] += 1
-      else
-        tile_unique_edge_count[tile_id] = 1
-      end
-    end
-  end
-
-  return tile_unique_edge_count.select{|tile, count| count == 4}.keys.reduce(&:*)
 end
 
 options = {
@@ -87,9 +66,93 @@ OptionParser.new do |opts|
 end.parse!(into: options)
 
 tiles = IO.read(options[:filename]).split("\n\n").map{|str| Tile::new(str)}
+tile_map = {}
+tiles.each do |t|
+  tile_map[t.id] = t
+end
+
+grid_size = Math.sqrt(tiles.length).to_i
+
+edge_to_tiles = {}
+tiles.each do |tile|
+  tile.edges.each do |edge|
+    edge_str = edge.join("")
+    if edge_to_tiles.key?(edge_str)
+      edge_to_tiles[edge_str].push(tile.id)
+    else
+      edge_to_tiles[edge_str] = [tile.id]
+    end
+  end
+end
+
+tile_unique_edge_count = {}
+edge_to_tiles.keys.each do |edge|
+  if edge_to_tiles[edge].size == 1
+    tile_id = edge_to_tiles[edge][0]
+    if tile_unique_edge_count.key?(tile_id)
+      tile_unique_edge_count[tile_id] += 1
+    else
+      tile_unique_edge_count[tile_id] = 1
+    end
+  end
+end
+
+corners = tile_unique_edge_count.select{|tile, count| count == 4}.keys
+#puts "Corners:"
+#puts corners
 
 if options[:part] == 1
-  puts part1(tiles)
-elsif options[:part] == 2
-  # do part 2
+  puts corners.reduce(&:*)
 end
+
+edges = tile_unique_edge_count.select{|tile, count| count == 2}.keys
+#puts "Edges:"
+#puts edges
+
+middles = tile_map.reject{ |k| corners.include?(k) or edges.include?(k) }.keys
+#puts "Middles:"
+#puts middles
+
+puzzle = []
+grid_size.times do |r|
+  row = []
+  grid_size.times do |c|
+    row.push(nil)
+  end
+  puzzle.push(row)
+end
+
+# Pick a corner
+puzzle[0][0] = tile_map[corners[0]]
+assigned_tile_ids = Set[corners[0]]
+(0..grid_size-1).each do |r|
+  (0..grid_size-1).each do |c|
+    if not puzzle[r][c].nil?
+      next
+    end
+
+    tile_to_match = nil
+    if r == 0
+      tile_to_match = puzzle[r][c - 1]
+    else
+      tile_to_match = puzzle[r - 1][c]
+    end
+
+    new_tile_id = nil
+    if (r == 0 or r == (grid_size - 1)) and (c == 0 or c == (grid_size - 1))
+      # We're picking from corners
+      new_tile_id = corners.find{|t| not assigned_tile_ids.member?(t) and tile_map[t].edges.any?{|te| tile_to_match.edges.any?{|me| te == me}}}
+    elsif (r == 0 or r == (grid_size - 1)) or (c == 0 or c == (grid_size - 1))
+      # We're picking from edges
+      new_tile_id = edges.find{|t| not assigned_tile_ids.member?(t) and tile_map[t].edges.any?{|te| tile_to_match.edges.any?{|me| te == me}}}
+    else
+      # We're picking from middles
+      new_tile_id = middles.find{|t| not assigned_tile_ids.member?(t) and tile_map[t].edges.any?{|te| tile_to_match.edges.any?{|me| te == me}}}
+    end
+
+    puzzle[r][c] = tile_map[new_tile_id]
+    assigned_tile_ids.add(new_tile_id)
+  end
+end
+
+print_puzzle_ids(puzzle)
