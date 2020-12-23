78545dbac9e44a0a17c35cb8762403717a13cb4d
Ganon11 <stark3@gmail.com>
2020-12-22 20:23:32 -0500

    I am so close and I do not care anymore.

--- 2020/Day20/src/Day20.rb
+++ 2020/Day20/src/Day20.rb
@@ -6,7 +6,7 @@ require 'set'
 class Tile
   attr_reader :id
   attr_reader :cols
-  attr_reader :edges
+  attr_reader :state
 
   def initialize(tile_string)
     lines = tile_string.split("\n")
@@ -18,10 +18,10 @@ class Tile
     end
 
     @grid = []
-    top = []
-    bottom = []
-    left = []
-    right = []
+    @top = []
+    @bottom = []
+    @left = []
+    @right = []
     lines.drop(1).each_with_index do |row, r|
       row.strip!
       gridrow = []
@@ -30,27 +30,263 @@ class Tile
         gridrow.push(value)
 
         if r == 0
-          top.push(value)
+          @top.push(value)
         elsif r == lines.length - 2
-          bottom.push(value)
+          @bottom.push(value)
         end
 
         if c == 0
-          left.push(value)
+          @left.push(value)
         elsif c == lines.length - 2
-          right.push(value)
+          @right.push(value)
         end
       end
       @grid.push(gridrow)
     end
 
-    @edges = [top, bottom, left, right, top.reverse, bottom.reverse, left.reverse, right.reverse]
+    @state = nil
+  end
+
+  def edges
+    [@top, @bottom, @left, @right, @top.reverse, @bottom.reverse, @left.reverse, @right.reverse]
+  end
+
+  def align_top_left_corner(corner_edges)
+    alignment_found = false
+    [:UP, :RIGHT, :LEFT, :DOWN, :UP_FLIPPED, :RIGHT_FLIPPED, :LEFT_FLIPPED, :DOWN_FLIPPED].each do |alignment|
+      @state = alignment
+      if corner_edges.include?(get_edge(:LEFT).join("")) and corner_edges.include?(get_edge(:UP).join(""))
+        alignment_found = true
+        break
+      end
+    end
+
+    alignment_found
+  end
+
+  def align_edge(direction, edge)
+    alignment_found = false
+    [:UP, :RIGHT, :LEFT, :DOWN, :UP_FLIPPED, :RIGHT_FLIPPED, :LEFT_FLIPPED, :DOWN_FLIPPED].each do |alignment|
+      @state = alignment
+      if get_edge(direction) == edge
+        alignment_found = true
+        break
+      end
+    end
+
+    alignment_found
+  end
+
+  def get_edge(direction)
+    if @state == :UP
+      case direction
+      when :UP then @top
+      when :RIGHT then @right
+      when :LEFT then @left
+      when :DOWN then @bottom
+      end
+    elsif @state == :RIGHT
+      case direction
+      when :UP then @left
+      when :RIGHT then @top
+      when :LEFT then @bottom
+      when :DOWN then @right
+      end
+    elsif @state == :LEFT
+      case direction
+      when :UP then @right
+      when :RIGHT then @bottom
+      when :LEFT then @top
+      when :DOWN then @left
+      end
+    elsif @state == :DOWN
+      case direction
+      when :UP then @bottom
+      when :RIGHT then @left
+      when :LEFT then @right
+      when :DOWN then @top
+      end
+    elsif @state == :UP_FLIPPED
+      case direction
+      when :UP then @top.reverse
+      when :RIGHT then @right.reverse
+      when :LEFT then @left.reverse
+      when :DOWN then @bottom.reverse
+      end
+    elsif @state == :RIGHT_FLIPPED
+      case direction
+      when :UP then @left.reverse
+      when :RIGHT then @top.reverse
+      when :LEFT then @bottom.reverse
+      when :DOWN then @right.reverse
+      end
+    elsif @state == :LEFT_FLIPPED
+      case direction
+      when :UP then @right.reverse
+      when :RIGHT then @bottom.reverse
+      when :LEFT then @top.reverse
+      when :DOWN then @left.reverse
+      end
+    elsif @state == :DOWN_FLIPPED
+      case direction
+      when :UP then @bottom.reverse
+      when :RIGHT then @left.reverse
+      when :LEFT then @right.reverse
+      when :DOWN then @top.reverse
+      end
+    end
+  end
+
+  def get_innards
+    copy = []
+    (0..@grid.length - 1).each do |r|
+      row = []
+      (0..@grid[r].length - 1).each do |c|
+        row.push(@grid[r][c])
+      end
+      copy.push(row)
+    end
+
+    if @state == :UP
+      # do nothing
+    elsif @state == :RIGHT
+      copy = copy.transpose.map(&:reverse)
+    elsif @state == :LEFT
+      copy = copy.transpose.map(&:reverse).transpose.map(&:reverse).transpose.map(&:reverse)
+    elsif @state == :DOWN
+      copy = copy.transpose.map(&:reverse).transpose.map(&:reverse)
+    elsif @state == :UP_FLIPPED
+      copy = copy.map(&:reverse)
+    elsif @state == :RIGHT_FLIPPED
+      copy = copy.map(&:reverse).transpose.map(&:reverse)
+    elsif @state == :LEFT_FLIPPED
+      copy = copy.map(&:reverse).transpose.map(&:reverse).transpose.map(&:reverse).transpose.map(&:reverse)
+    elsif @state == :DOWN_FLIPPED
+      copy = copy.map(&:reverse).transpose.map(&:reverse).transpose.map(&:reverse)
+    end
+
+    innards = copy[1..-2]
+    copy.each_with_index do |row, index|
+      if index == 0 or index = copy.length - 1
+        next
+      end
+      innards[index] = copy[row][1..-2]
+    end
+
+    return innards
   end
 end
 
 def print_puzzle_ids(puzzle)
   puzzle.each do |row|
-    puts row.map{|tile| tile.nil? ? "?" : tile.id}.join(", ")
+    puts row.map{|tile| tile.id}.join(", ")
+  end
+end
+
+def print_puzzle_alignments(puzzle)
+  puzzle.each do |row|
+    puts row.map{|tile| tile.state}.join(", ")
+  end
+end
+
+def dump_puzzle_to_grid(puzzle, grid_size)
+  grid = {}
+  max_row = -1
+  max_col = -1
+  (0..grid_size-1).each do |r|
+    (0..grid_size-1).each do |c|
+      innards = puzzle[r][c].get_innards
+      innards.each_with_index do |_, r2|
+        innards[r2].each_with_index do |value, c2|
+          actual_row = (r * innards.length) + r2
+          actual_column = (c * innards[r2].length) + c2
+          grid["#{actual_row},#{actual_column}"] = value
+          if actual_row > max_row
+            max_row = actual_row
+          end
+
+          if actual_column > max_col
+            max_col = actual_column
+          end
+        end
+      end
+    end
+  end
+
+  actual_grid = []
+  (0..max_row).each do |r|
+    row = []
+    (0..max_col).each do |c|
+      row.push(grid["#{r},#{c}"])
+    end
+    actual_grid.push(row)
+  end
+
+  return actual_grid
+end
+
+def print_grid(grid)
+  grid.each do |row|
+    row.each do |value|
+      if value == 0
+        print ' '
+      elsif value == 1
+        print '#'
+      else
+        print value
+      end
+    end
+    puts ""
+  end
+end
+
+def shape_at_position?(grid, shape, row, col)
+  shape.each_with_index do |shape_row, sr|
+    shape_row.each_with_index do |value, sc|
+      if value == 1 and grid[row + sr][col + sc] != 1
+        return false
+      end
+    end
+  end
+
+  return true
+end
+
+def find_shape(grid, shape)
+  shape_height = shape.length
+  shape_width = shape[0].length
+  positions = []
+
+  grid.each_with_index do |row, r|
+    if r + shape_height >= grid.length
+      next
+    end
+
+    row.each_with_index do |_, c|
+      if c + shape_width >= row.length
+        next
+      end
+
+      if shape_at_position?(grid, shape, r, c)
+        positions.push([r, c])
+      end
+    end
+  end
+
+  return positions
+end
+
+def rotate_grid(grid)
+  return grid.transpose.map(&:reverse)
+end
+
+def mask_shape(grid, shape, row, col)
+  shape.each_with_index do |shape_row, sr|
+    shape_row.each_with_index do |value, sc|
+      if value == 1
+        grid[row + sr][col + sc] = 'O'
+      end
+    end
   end
 end
 
@@ -155,4 +391,56 @@ assigned_tile_ids = Set[corners[0]]
   end
 end
 
-print_puzzle_ids(puzzle)
+#print_puzzle_ids(puzzle)
+
+corner_edges = edge_to_tiles.keys.filter{|e| edge_to_tiles[e].size == 1}
+#puts corner_edges
+
+puzzle[0][0].align_top_left_corner(corner_edges)
+#puts puzzle[0][0].state
+(0..grid_size-1).each do |r|
+  (0..grid_size-1).each do |c|
+    if r == 0
+      tile_to_match = puzzle[r][c - 1]
+      puzzle[r][c].align_edge(:LEFT, tile_to_match.get_edge(:RIGHT))
+    else
+      tile_to_match = puzzle[r - 1][c]
+      puzzle[r][c].align_edge(:UP, tile_to_match.get_edge(:DOWN))
+    end
+  end
+end
+
+#print_puzzle_alignments(puzzle)
+grid = dump_puzzle_to_grid(puzzle, grid_size)
+#print_grid(grid)
+
+
+shape = [
+  '                  # ',
+  '#    ##    ##    ###',
+  ' #  #  #  #  #  #   '
+]
+
+shape = shape.map{ |line| line.chars.map{ |c| c == '#' ? 1 : 0}}
+
+grid = grid.map(&:reverse)
+grid = rotate_grid(grid)
+grid = rotate_grid(grid)
+grid = rotate_grid(grid)
+positions = find_shape(grid, shape)
+
+positions.each do |p|
+  mask_shape(grid, shape, p[0], p[1])
+end
+
+print_grid(grid)
+
+sum = 0
+grid.each do |row|
+  sum += row.count {|v| v == 1}
+end
+
+puts sum
+# positions.each do |p|
+#   puts "#{p[0]},#{p[1]}"
+# end
