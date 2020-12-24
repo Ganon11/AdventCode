9fb11d29c28169e0e286e19d5d83f02296ca985e
Ganon11 <stark3@gmail.com>
2020-12-24 17:54:33 -0500

    More cleanup. Most significantly, switching to axial coordinates gains a bit of time as there are only two values being hashed.

--- 2020/Day24/src/Day24.rb
+++ 2020/Day24/src/Day24.rb
@@ -5,45 +5,39 @@ require 'set'
 
 def get_hex_coordinates(direction)
   i = 0
-  x = 0
-  y = 0
-  z = 0
+  q = 0
+  r = 0
   while i < direction.length
     if direction[i] == 'e'
-      x += 1
-      y -= 1
+      q += 1
       i += 1
     elsif direction[i] == 'w'
-      x -= 1
-      y += 1
+      q -= 1
       i += 1
     elsif direction[i] == 's' and direction[i + 1] == 'e'
-      y -= 1
-      z += 1
+      r += 1
       i += 2
     elsif direction[i] == 's' and direction[i + 1] == 'w'
-      x -= 1
-      z += 1
+      q -= 1
+      r += 1
       i += 2
     elsif direction[i] == 'n' and direction[i + 1] == 'e'
-      x += 1
-      z -= 1
+      q += 1
+      r -= 1
       i += 2
     elsif direction[i] == 'n' and direction[i + 1] == 'w'
-      y += 1
-      z -= 1
+      r -= 1
       i += 2
     end
   end
 
-  return x, y, z
+  return [q, r]
 end
 
 def get_initial_grid(directions)
   grid = Set[]
   directions.each do |direction|
-    x, y, z = get_hex_coordinates(direction)
-    key = [x, y, z]
+    key = get_hex_coordinates(direction)
     grid.delete(key) unless grid.add?(key)
   end
 
@@ -51,25 +45,18 @@ def get_initial_grid(directions)
 end
 
 def get_neighbors(hex)
-  n1 = [hex[0] + 1, hex[1] - 1, hex[2]]
-  n2 = [hex[0], hex[1] - 1, hex[2] + 1]
-  n3 = [hex[0] - 1, hex[1], hex[2] + 1]
-  n4 = [hex[0] - 1, hex[1] + 1, hex[2]]
-  n5 = [hex[0], hex[1] + 1, hex[2] - 1]
-  n6 = [hex[0] + 1, hex[1], hex[2] - 1]
+  n1 = [hex[0] + 1, hex[1] - 1]
+  n2 = [hex[0] + 1, hex[1]]
+  n3 = [hex[0], hex[1] + 1]
+  n4 = [hex[0] - 1, hex[1] + 1]
+  n5 = [hex[0] - 1, hex[1]]
+  n6 = [hex[0], hex[1] - 1]
 
   return [n1, n2, n3, n4, n5, n6]
 end
 
 def count_active_neighbors(grid, hex)
-  sum = 0
-  get_neighbors(hex).each do |n|
-    if grid.include?(n)
-      sum += 1
-    end
-  end
-
-  return sum
+  return get_neighbors(hex).count {|n| grid.include?(n)}
 end
 
 def play_round(grid)
@@ -78,16 +65,12 @@ def play_round(grid)
   hexes_to_check = Set[]
   grid.each do |hex|
     hexes_to_check.add(hex)
-    get_neighbors(hex).each do |n|
-      hexes_to_check.add(n)
-    end
+    hexes_to_check.merge(get_neighbors(hex))
   end
 
   hexes_to_check.each do |hex|
     count = count_active_neighbors(grid, hex)
-    if grid.include?(hex) and (count == 1 or count == 2)
-      new_grid.add(hex)
-    elsif count == 2
+    if count == 2 or (grid.include?(hex) and count == 1)
       new_grid.add(hex)
     end
   end
