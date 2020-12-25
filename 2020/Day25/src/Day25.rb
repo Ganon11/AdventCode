610b20871903e63ad4ff6153985f9b5cf987a6c9
Ganon11 <stark3@gmail.com>
2020-12-25 09:26:51 -0500

    Day 25 done. Misread the end of the instructions, didn't realize that the public key was used as the subject number, and not an initial value.

--- 2020/Day25/src/Day25.rb
+++ 2020/Day25/src/Day25.rb
@@ -2,19 +2,48 @@
 
 require 'optparse'
 
+$SUBJECT_NUMBER = 7
+$MODULO_VALUE = 20201227
+
+def transform_value(value, subject_number = $SUBJECT_NUMBER)
+  return (value * subject_number) % $MODULO_VALUE
+end
+
+def find_loop_size(target, subject_number = $SUBJECT_NUMBER)
+  count = 0
+  value = 1
+  while value != target
+    count += 1
+    value = transform_value(value, subject_number)
+  end
+
+  return count
+end
+
+def do_transform(loop_size, subject_number = $SUBJECT_NUMBER)
+  value = 1
+  loop_size.times do
+    value = transform_value(value, subject_number)
+  end
+
+  return value
+end
+
+def get_encryption_key(card, door)
+  door_loop_size = find_loop_size(door)
+  answer = do_transform(door_loop_size, card)
+  return answer
+end
+
 options = {
-  :part => 1,
   :filename => "../input/sample.txt"
 }
 
 OptionParser.new do |opts|
   opts.banner = "Usage: Day25.rb [options]"
-  opts.on("-p PART", "--part=PART", Integer, "Part 1 or 2")
   opts.on("-f FILENAME", "--filename=FILENAME", String, "Which filename to use?")
 end.parse!(into: options)
 
-if options[:part] == 1
-  # do part 1
-elsif options[:part] == 2
-  # do part 2
-end
+card, door = IO.readlines(options[:filename]).map{|l| l.strip.to_i}
+
+puts get_encryption_key(card, door)
