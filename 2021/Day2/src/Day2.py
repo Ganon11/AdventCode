6908e093e391e53946232b6cfd6d56b0a307e588
Ganon11 <stark3@gmail.com>
2021-12-09 15:29:03 -0500

    Cleanup and linter fixes.

--- 2021/Day2/src/Day2.py
+++ 2021/Day2/src/Day2.py
@@ -1,6 +1,7 @@
 import argparse
 
 def get_instructions(filename):
+  '''Gets a list of submarine instructions from the file.'''
   instructions = []
   with open(filename, 'r') as file:
     instructions = [line.strip().split(' ') for line in file.readlines()]
@@ -8,23 +9,25 @@ def get_instructions(filename):
   return instructions
 
 def follow_naive_instructions(instructions):
+  '''Follows the instructions without aim.'''
   horizontal_position = 0
   depth = 0
 
   for instruction in instructions:
     value = int(instruction[1])
     if instruction[0] == 'forward':
-      horizontal_position = horizontal_position + value
+      horizontal_position += value
     elif instruction[0] == 'down':
-      depth = depth + value
+      depth += value
     elif instruction[0] == 'up':
-      depth = depth - value
+      depth -= value
     else:
       raise ValueError(f'Unknown instruction: {instruction[0]}')
 
   return (horizontal_position, depth)
 
 def follow_complicated_instructions(instructions):
+  '''Follows the instructions with aim.'''
   horizontal_position = 0
   depth = 0
   aim = 0
@@ -32,18 +35,19 @@ def follow_complicated_instructions(instructions):
   for instruction in instructions:
     value = int(instruction[1])
     if instruction[0] == 'forward':
-      horizontal_position = horizontal_position + value
-      depth = depth + (value * aim)
+      horizontal_position += value
+      depth += value * aim
     elif instruction[0] == 'down':
-      aim = aim + value
+      aim += value
     elif instruction[0] == 'up':
-      aim = aim - value
+      aim -= value
     else:
       raise ValueError(f'Unknown instruction: {instruction[0]}')
 
   return (horizontal_position, depth)
 
 def main():
+  '''Deeper we must go!'''
   parser = argparse.ArgumentParser()
   parser.add_argument('-f', '--filename', default='../input/sample.txt')
   parser.add_argument('-p', '--part', choices=[1, 2], default=1, type=int)
