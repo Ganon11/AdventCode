677fc34a3e7a72beb0facacd8bf2d6e3aa5f2031
Ganon11 <stark3@gmail.com>
2021-12-13 11:57:33 -0500

    Day 13 cleanup.

--- 2021/Day13/src/Day13.py
+++ 2021/Day13/src/Day13.py
@@ -1,34 +1,30 @@
 import argparse
+from collections import namedtuple
 import re
 
 from position import Position # My own 2019 code!
 
 def get_grid_and_folds(filename):
   '''Gets a grid of dots and collection of folds from the file.'''
-  lines = []
-  with open(filename, 'r') as fh:
-    lines = list(map(lambda line: line.strip(), fh.readlines()))
-
   parsing_dots = True
-  index = 0
   grid = dict()
+  Fold = namedtuple('Fold', 'axis value')
   folds = list()
   pattern = re.compile(r'fold along (x|y)=(\d+)', re.IGNORECASE)
-  while index < len(lines):
-    line = lines[index]
-    if parsing_dots:
-      if len(line) == 0:
-        parsing_dots = False
+  with open(filename, 'r') as fh:
+    for line in fh.readlines():
+      line = line.strip()
+      if parsing_dots:
+        if len(line) == 0:
+          parsing_dots = False
+        else:
+          (x, y) = map(int, line.split(','))
+          pos = Position(x, y)
+          grid[pos] = 1
       else:
-        (x, y) = map(int, line.split(','))
-        pos = Position(x, y)
-        grid[pos] = 1
-    else:
-      match = pattern.match(line)
-      axis = match.group(1)
-      value = int(match.group(2))
-      folds.append((axis, value))
-    index += 1
+        match = pattern.match(line)
+        folds.append(Fold(match.group(1), int(match.group(2))))
+
   return (grid, folds)
 
 def print_grid(grid):
@@ -47,26 +43,19 @@ def print_grid(grid):
         print(' ', end='')
     print('', flush=True)
 
-def count_grid(grid):
-  '''Counts the number of visible dots.'''
-  return len(grid.keys())
-
 def execute_fold(grid, fold):
   '''Folds the paper along a line.'''
-  axis = fold[0]
-  value = fold[1]
-
   new_positions = []
   positions_to_delete = []
   for position, _ in grid.items():
-    if axis == 'x' and position.x > value:
+    if fold.axis == 'x' and position.x > fold.value:
       positions_to_delete.append(position)
-      new_x = value - abs(value - position.x)
+      new_x = fold.value - abs(fold.value - position.x)
       new_position = Position(new_x, position.y)
       new_positions.append(new_position)
-    elif axis == 'y' and position.y > value:
+    elif fold.axis == 'y' and position.y > fold.value:
       positions_to_delete.append(position)
-      new_y = value - abs(value - position.y)
+      new_y = fold.value - abs(fold.value - position.y)
       new_position = Position(position.x, new_y)
       new_positions.append(new_position)
 
@@ -75,8 +64,6 @@ def execute_fold(grid, fold):
 
   for position in positions_to_delete:
     del grid[position]
-
-  return grid
 
 def main():
   '''I hope it makes a swan!'''
@@ -87,11 +74,11 @@ def main():
 
   (grid, folds) = get_grid_and_folds(args.filename)
   if args.part == 1:
-    grid = execute_fold(grid, folds[0])
-    print(f'After 1 fold, number of dots: {count_grid(grid)}')
+    execute_fold(grid, folds[0])
+    print(f'After 1 fold, number of dots: {len(grid)}')
   elif args.part == 2:
     for fold in folds:
-      grid = execute_fold(grid, fold)
+      execute_fold(grid, fold)
     print_grid(grid)
 
 if __name__ == "__main__":
