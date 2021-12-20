970126b6429f0c2e2f8091c72dc6450da410bf5d
Ganon11 <stark3@gmail.com>
2021-12-20 17:16:26 -0500

    Massive optimization. Turns out calling min() and max() over massive grids is slow.

--- 2021/Day20/src/Day20.py
+++ 2021/Day20/src/Day20.py
@@ -4,79 +4,60 @@ def get_trench_map(filename):
   '''Get the image enhancement string and grid from the file.'''
   image_enhancement_algorithm = None
   grid = dict()
+  max_x = 0
+  max_y = 0
   with open(filename, 'r') as fh:
     image_enhancement_algorithm = fh.readline().strip()
     fh.readline()
     line = fh.readline().strip()
-    y = 0
+    max_x = len(line)
     while line:
       for x, ch in enumerate(line):
         if ch == '#':
           value = '#'
         else:
           value = ' '
-        grid[(x, y)] = value
+        grid[(x, max_y)] = value
 
       line = fh.readline().strip()
-      y += 1
+      max_y += 1
 
-  return (image_enhancement_algorithm, grid)
-
-def print_grid(grid):
-  '''Prints a grid.'''
-  minx = min(grid.keys(), key=lambda p: p[0])[0]
-  maxx = max(grid.keys(), key=lambda p: p[0])[0]
-  miny = min(grid.keys(), key=lambda p: p[1])[1]
-  maxy = max(grid.keys(), key=lambda p: p[1])[1]
-
-  for y in range(miny, maxy + 1):
-    for x in range(minx, maxx + 1):
-      print(grid[(x, y)], end='')
-    print('', flush=True)
-
-def get_ordered_neighbors(x, y):
-  return [
-    (x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
-    (x - 1, y), (x, y), (x + 1, y),
-    (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
-  ]
+  return (image_enhancement_algorithm, grid, max_x, max_y)
 
 def get_index(x, y, grid, infinite_value):
-  minx = min(grid.keys(), key=lambda p: p[0])[0] - 1
-  maxx = max(grid.keys(), key=lambda p: p[0])[0] + 1
-  miny = min(grid.keys(), key=lambda p: p[1])[1] - 1
-  maxy = max(grid.keys(), key=lambda p: p[1])[1] + 1
-  digits = []
-  for neighbor in get_ordered_neighbors(x, y):
-    if neighbor not in grid:
-      digits.append(infinite_value)
-    else:
-      if grid[neighbor] == '#':
-        digits.append('1')
-      else:
-        digits.append('0')
+  digits = ''
+  neighbors = None
+  if (x, y) in get_index.PRECOMPUTED_NEIGHBORS:
+    neighbors = get_index.PRECOMPUTED_NEIGHBORS[(x, y)]
+  else:
+    neighbors = [
+      (x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
+      (x - 1, y),     (x, y),     (x + 1, y),
+      (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+    ]
+    get_index.PRECOMPUTED_NEIGHBORS[(x, y)] = neighbors
 
-  return int(''.join(digits), 2)
+  for neighbor in neighbors:
+    if neighbor not in grid:
+      digits += infinite_value
+    elif grid[neighbor] == '#':
+      digits += '1'
+    else:
+      digits += '0'
+
+  return int(digits, 2)
+
+get_index.PRECOMPUTED_NEIGHBORS = dict()
 
 def count_pixels(grid):
-  minx = min(grid.keys(), key=lambda p: p[0])[0]
-  maxx = max(grid.keys(), key=lambda p: p[0])[0]
-  miny = min(grid.keys(), key=lambda p: p[1])[1]
-  maxy = max(grid.keys(), key=lambda p: p[1])[1]
-  total = 0
-  for y in range(miny, maxy + 1):
-    for x in range(minx, maxx + 1):
-      if grid[(x, y)] == '#':
-        total += 1
+  return sum(1 for p in grid.keys() if grid[p] == '#')
 
-  return total
-
-def do_step(grid, step, iea):
+def do_step(grid, step, iea, dimensions):
   new_grid = dict()
-  minx = min(grid.keys(), key=lambda p: p[0])[0] - 1
-  maxx = max(grid.keys(), key=lambda p: p[0])[0] + 1
-  miny = min(grid.keys(), key=lambda p: p[1])[1] - 1
-  maxy = max(grid.keys(), key=lambda p: p[1])[1] + 1
+  minx = dimensions[0] - 1
+  miny = dimensions[1] - 1
+  maxx = dimensions[2] + 1
+  maxy = dimensions[3] + 1
 
   if iea[0] == '.':
     infinite_value = '0'
@@ -85,14 +66,11 @@ def do_step(grid, step, iea):
   else:
     infinite_value = '0'
 
-  print(f'Doing step {step}, the infinite value is {infinite_value}')
-
   for y in range(miny, maxy + 1):
     for x in range(minx, maxx + 1):
-      index = get_index(x, y, grid, infinite_value)
-      new_grid[(x, y)] = iea[index]
+      new_grid[(x, y)] = iea[get_index(x, y, grid, infinite_value)]
 
-  return new_grid
+  return (new_grid, (minx, miny, maxx, maxy))
 
 def main():
   parser = argparse.ArgumentParser()
@@ -100,19 +78,20 @@ def main():
   parser.add_argument('-p', '--part', choices=[1, 2], default=1, type=int)
   args = parser.parse_args()
 
-  (image_enhancement_algorithm, grid) = get_trench_map(args.filename)
+  (image_enhancement_algorithm, grid, maxx, maxy) = get_trench_map(args.filename)
 
   if args.part == 1:
     steps = 2
   elif args.part == 2:
     steps = 50
 
-  for step in range(steps):
-    grid = do_step(grid, step + 1, image_enhancement_algorithm)
+  dimensions = (0, 0, maxx, maxy)
 
-  #print_grid(grid)
+  for step in range(steps):
+    (grid, dimensions) = do_step(grid, step + 1, image_enhancement_algorithm, dimensions)
+
   total = count_pixels(grid)
-  print(f'There are {total} pixels on.')
+  print(f'After {steps} steps, there are {total} pixels on.')
 
 if __name__ == "__main__":
   main()
