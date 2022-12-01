efdeb4ac5c4db1754553d251cf2b3d06cb6cd257
Ganon11 <stark3@gmail.com>
2022-11-30 23:31:22 -0500

    style: use file-level namespace

--- 2022/PositionTests/ConstructorTests.cs
+++ 2022/PositionTests/ConstructorTests.cs
@@ -1,42 +1,41 @@
-﻿namespace PositionTests
-{
-   [TestClass]
-   public class ConstructorTests
-   {
-      [TestMethod]
-      public void Test_Default_Constructor()
-      {
-         Position.Position p = new();
-         Assert.AreEqual(0, p.X);
-         Assert.AreEqual(0, p.Y);
-         Assert.AreEqual(0, p.Z);
-      }
+﻿namespace PositionTests;
 
-      [TestMethod]
-      public void Test_Constructor_One_Argument()
-      {
-         Position.Position p = new(1);
-         Assert.AreEqual(1, p.X);
-         Assert.AreEqual(0, p.Y);
-         Assert.AreEqual(0, p.Z);
-      }
+ [TestClass]
+ public class ConstructorTests
+ {
+    [TestMethod]
+    public void Test_Default_Constructor()
+    {
+       Position.Position p = new();
+       Assert.AreEqual(0, p.X);
+       Assert.AreEqual(0, p.Y);
+       Assert.AreEqual(0, p.Z);
+    }
 
-      [TestMethod]
-      public void Test_Constructor_Two_Arguments()
-      {
-         Position.Position p = new(1, 2);
-         Assert.AreEqual(1, p.X);
-         Assert.AreEqual(2, p.Y);
-         Assert.AreEqual(0, p.Z);
-      }
+    [TestMethod]
+    public void Test_Constructor_One_Argument()
+    {
+       Position.Position p = new(1);
+       Assert.AreEqual(1, p.X);
+       Assert.AreEqual(0, p.Y);
+       Assert.AreEqual(0, p.Z);
+    }
 
-      [TestMethod]
-      public void Test_Constructor_Three_Arguments()
-      {
-         Position.Position p = new(1, 2, 3);
-         Assert.AreEqual(1, p.X);
-         Assert.AreEqual(2, p.Y);
-         Assert.AreEqual(3, p.Z);
-      }
-   }
-}
+    [TestMethod]
+    public void Test_Constructor_Two_Arguments()
+    {
+       Position.Position p = new(1, 2);
+       Assert.AreEqual(1, p.X);
+       Assert.AreEqual(2, p.Y);
+       Assert.AreEqual(0, p.Z);
+    }
+
+    [TestMethod]
+    public void Test_Constructor_Three_Arguments()
+    {
+       Position.Position p = new(1, 2, 3);
+       Assert.AreEqual(1, p.X);
+       Assert.AreEqual(2, p.Y);
+       Assert.AreEqual(3, p.Z);
+    }
+ }
