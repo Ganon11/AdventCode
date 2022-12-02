07da91a9fdbb51b07bc36d8fa209d0d347ef97ab
Ganon11 <stark3@gmail.com>
2022-12-02 17:58:30 -0500

    chore: add day scaffolding

---
+++ 2022/AoC2022/Day16.cs
@@ -0,0 +1,15 @@
+namespace Ganon11.AdventOfCode.Year2022;
+using Tidy.AdventOfCode;
+
+internal sealed class Day2 : Day
+{
+   public override object ExecutePart1()
+   {
+      return null;
+   }
+
+   public override object ExecutePart2()
+   {
+      return null;
+   }
+}
