b20c6ac910e5f26e85e6e4fd800423632a2602dc
Ganon11 <stark3@gmail.com>
2022-12-02 16:40:09 -0500

    chore: remove old program.cs

--- 2022/Program.cs
+++
@@ -1,9 +0,0 @@
-// See https://aka.ms/new-console-template for more information
-
-internal class AoC2022
-{
-   static void Main(string[] args)
-   {
-      await Tidy.AdventOfCode.Runner.CreateDefault().ExecuteAsync();
-   }
-}
