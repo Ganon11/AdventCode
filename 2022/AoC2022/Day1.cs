64f4d9c88f60348a59189a271667c45d524f472e
Ganon11 <stark3@gmail.com>
2022-12-02 19:31:33 -0500

    chore: clean up code with templated day inheritance

--- 2022/AoC2022/Day1.cs
+++ 2022/AoC2022/Day1.cs
@@ -1,28 +1,14 @@
 namespace Ganon11.AdventOfCode.Year2022;
 using Tidy.AdventOfCode;
 
-internal sealed class Day1 : Day
+internal sealed class Day1 : Day<List<int>>
 {
-   private static List<int> GetElves(string input)
-   {
-      var elves = new List<int>();
-      var groups = input.Split("\n\n");
-      foreach (var group in groups)
-      {
-         var lines = group.Split('\n');
-         var total = 0;
-         foreach (var line in lines)
-         {
-            total += int.Parse(line, System.Globalization.CultureInfo.CurrentCulture);
-         }
+   public override List<int> ParseInput(string rawInput) => rawInput
+      .Split("\n\n")
+      .Select(g => g.Split("\n").Select(l => int.Parse(l, System.Globalization.CultureInfo.CurrentCulture)).Sum())
+      .ToList();
 
-         elves.Add(total);
-      }
+   public override object ExecutePart1() => this.Input.Max();
 
-      return elves;
-   }
-
-   public override object ExecutePart1() => GetElves(this.Input).Max();
-
-   public override object ExecutePart2() => GetElves(this.Input).OrderByDescending(x => x).Take(3).Sum();
+   public override object ExecutePart2() => this.Input.OrderByDescending(x => x).Take(3).Sum();
 }
