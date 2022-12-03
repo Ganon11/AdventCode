557692681fbc886c42e93d66dfdfbe96a756f6f5
Ganon11 <stark3@gmail.com>
2022-12-02 21:26:51 -0500

    day(2): simplify code

--- 2022/AoC2022/Day02.cs
+++ 2022/AoC2022/Day02.cs
@@ -3,40 +3,23 @@ using Tidy.AdventOfCode;
 
 internal sealed class Day2 : Day<string[][]>
 {
-   private static int CalculateScore(string[][] input, Dictionary<char, Dictionary<char, int>> lookup)
-   {
-      var score = 0;
-      foreach (var line in input)
-      {
-         score += lookup[line[0][0]][line[1][0]];
-      }
-
-      return score;
-   }
-
    public override string[][] ParseInput(string rawInput) => rawInput.Split("\n").Select(line => line.Split(" ")).ToArray();
 
-   public override object ExecutePart1()
+   private static Dictionary<string, Dictionary<string, int>> p1 = new()
    {
-      Dictionary<char, Dictionary<char, int>> lookup = new()
-      {
-         { 'A', new() { { 'X', 4 }, { 'Y', 8 }, { 'Z', 3 } } },
-         { 'B', new() { { 'X', 1 }, { 'Y', 5 }, { 'Z', 9 } } },
-         { 'C', new() { { 'X', 7 }, { 'Y', 2 }, { 'Z', 6 } } }
-      };
+      { "A", new() { { "X", 4 }, { "Y", 8 }, { "Z", 3 } } },
+      { "B", new() { { "X", 1 }, { "Y", 5 }, { "Z", 9 } } },
+      { "C", new() { { "X", 7 }, { "Y", 2 }, { "Z", 6 } } }
+   };
 
-      return CalculateScore(this.Input, lookup);
-   }
+   public override object ExecutePart1() => this.Input.Select(line => p1[line[0]][line[1]]).Sum();
 
-   public override object ExecutePart2()
+   private static Dictionary<string, Dictionary<string, int>> p2 = new()
    {
-      Dictionary<char, Dictionary<char, int>> lookup = new()
-      {
-         { 'A', new() { { 'X', 3 }, { 'Y', 4 }, { 'Z', 8 } } },
-         { 'B', new() { { 'X', 1 }, { 'Y', 5 }, { 'Z', 9 } } },
-         { 'C', new() { { 'X', 2 }, { 'Y', 6 }, { 'Z', 7 } } }
-      };
+      { "A", new() { { "X", 3 }, { "Y", 4 }, { "Z", 8 } } },
+      { "B", new() { { "X", 1 }, { "Y", 5 }, { "Z", 9 } } },
+      { "C", new() { { "X", 2 }, { "Y", 6 }, { "Z", 7 } } }
+   };
 
-      return CalculateScore(this.Input, lookup);
-   }
+   public override object ExecutePart2() => this.Input.Select(line => p2[line[0]][line[1]]).Sum();
 }
