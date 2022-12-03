843a9d8a5d9bdc092e1d15c842871efcb2056f50
Ganon11 <stark3@gmail.com>
2022-12-02 19:27:45 -0500

    chore: suppress warning on program class

--- 2022/AoC2022/Program.cs
+++ 2022/AoC2022/Program.cs
@@ -1,2 +1,8 @@
 // See https://aka.ms/new-console-template for more information
+
+// CA1852 Type 'Program' can be sealed because it has no subtypes in its containing assembly and is not externally visible
+#pragma warning disable CA1852
+
 await Tidy.AdventOfCode.Runner.CreateDefault().ExecuteAsync();
+
+#pragma warning restore CA1852
