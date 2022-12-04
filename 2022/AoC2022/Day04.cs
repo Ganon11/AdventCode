bf511150590998928bd5dfe6201600b6fc63a8ea
Ganon11 <stark3@gmail.com>
2022-12-04 14:07:36 -0500

    day(4): add solutions

--- 2022/AoC2022/Day04.cs
+++ 2022/AoC2022/Day04.cs
@@ -1,9 +1,33 @@
-// namespace Ganon11.AdventOfCode.Year2022;
-// using Tidy.AdventOfCode;
+namespace Ganon11.AdventOfCode.Year2022;
+using Tidy.AdventOfCode;
 
-// internal sealed class Day4 : Day
-// {
-//    public override object ExecutePart1() => -1;
+using RangePair = Tuple<HashSet<int>, HashSet<int>>;
 
-//    public override object ExecutePart2() => -1;
-// }
+internal sealed class Day4 : Day<IEnumerable<RangePair>>
+{
+   private static HashSet<int> ConvertRangeStringToSet(string range)
+   {
+      var pair = range.Split('-')
+         .Select(i => int.Parse(i, System.Globalization.CultureInfo.CurrentCulture))
+         .ToArray();
+      return new HashSet<int>(Enumerable.Range(pair[0], pair[1] - pair[0] + 1));
+   }
+
+   public override IEnumerable<RangePair> ParseInput(string rawInput) => rawInput.Split('\n')
+      .Select(line =>
+      {
+         var ranges = line.Split(',');
+         return new RangePair(
+            ConvertRangeStringToSet(ranges[0]),
+            ConvertRangeStringToSet(ranges[1])
+         );
+      });
+
+   public override object ExecutePart1() => this.Input
+      .Count(pair => pair.Item1.IsProperSubsetOf(pair.Item2)
+         || pair.Item2.IsProperSubsetOf(pair.Item1)
+         || pair.Item1.SetEquals(pair.Item2));
+
+   public override object ExecutePart2() => this.Input
+      .Count(pair => pair.Item1.Intersect(pair.Item2).Any());
+}
