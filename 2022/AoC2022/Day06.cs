17815045bf5b9687af4617e3d0cf0ba86358c997
Mike Stark <mstark@factset.com>
2022-12-06 09:53:51 -0500

    day(6): adding queue-based solution

--- 2022/AoC2022/Day06.cs
+++ 2022/AoC2022/Day06.cs
@@ -1,9 +1,38 @@
-// namespace Ganon11.AdventOfCode.Year2022;
-// using Tidy.AdventOfCode;
+namespace Ganon11.AdventOfCode.Year2022;
+using Tidy.AdventOfCode;
 
-// internal sealed class Day6 : Day
-// {
-//    public override object ExecutePart1() => -1;
+internal sealed class Day6 : Day
+{
+   private static int FindMarker(string input, int targetLength)
+   {
+      if (input.Length < targetLength)
+      {
+         return -1;
+      }
 
-//    public override object ExecutePart2() => -1;
-// }
+      var marker = new Queue<char>();
+      var answer = 0;
+      while (answer < targetLength)
+      {
+         marker.Enqueue(input[answer++]);
+      }
+
+      while (answer < input.Length)
+      {
+         if (marker.SequenceEqual(marker.Distinct()))
+         {
+            return answer;
+         }
+
+         _ = marker.Dequeue();
+         marker.Enqueue(input[answer]);
+         ++answer;
+      }
+
+      return -1;
+   }
+
+   public override object ExecutePart1() => FindMarker(this.Input, 4);
+
+   public override object ExecutePart2() => FindMarker(this.Input, 14);
+}
