85111cad190f637de36963fb7f7c7ce50eddc17e
Mike Stark <mstark@factset.com>
2022-12-07 11:51:03 -0500

    chore: add day names to all solutions

--- CSharp/AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ CSharp/AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-   public Solution() : base(01, 2022, "") { }
+   public Solution() : base(01, 2022, "Calorie Counting") { }
 
    protected override string SolvePartOne()
    {
