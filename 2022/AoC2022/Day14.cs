96bcbc870a38f573ed97bbdd67db5a2e626e8445
Ganon11 <stark3@gmail.com>
2022-12-07 13:47:47 -0500

    chore: re-arranged code, csharp is now at root level

--- 2022/AoC2022/Day14.cs
+++
@@ -1,9 +0,0 @@
-// namespace Ganon11.AdventOfCode.Year2022;
-// using Tidy.AdventOfCode;
-
-// internal sealed class Day14 : Day
-// {
-//    public override object ExecutePart1() => -1;
-
-//    public override object ExecutePart2() => -1;
-// }
