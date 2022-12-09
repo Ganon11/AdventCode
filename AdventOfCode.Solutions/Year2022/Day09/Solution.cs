fa576f4a00b99838f66785d99e6e8fbacac99dc8
Ganon11 <stark3@gmail.com>
2022-12-09 10:02:11 -0500

    day(9): cleanup, consolidate code

--- AdventOfCode.Solutions/Year2022/Day09/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day09/Solution.cs
@@ -1,5 +1,7 @@
 namespace AdventOfCode.Solutions.Year2022.Day09;
 
+using AdventOfCode.Position;
+
 internal class Solution : SolutionBase<Tuple<char, int>[]>
 {
    public Solution() : base(09, 2022, "Rope Bridge") { }
@@ -12,154 +14,88 @@ internal class Solution : SolutionBase<Tuple<char, int>[]>
       })
       .ToArray();
 
-   public override string SolvePartOne()
+   private static void MoveRope(Position[] positions, char direction)
    {
-      var head = new Position.Position();
-      var tail = new Position.Position();
-
-      var tailPositions = new HashSet<Position.Position>
+      switch (direction)
       {
-         tail
-      };
-
-      foreach (var instruction in this.ParsedInput)
-      {
-         for (var count = 0; count < instruction.Item2; ++count)
-         {
-            switch (instruction.Item1)
-            {
-               case 'U':
-                  head = head.North;
-                  break;
-               case 'D':
-                  head = head.South;
-                  break;
-               case 'L':
-                  head = head.West;
-                  break;
-               case 'R':
-                  head = head.East;
-                  break;
-               default:
-                  break;
-            }
-
-            if (head.X == tail.X && (head.Y - 2) == tail.Y)
-            {
-               tail = tail.North;
-            }
-            else if (head.X == tail.X && (head.Y + 2) == tail.Y)
-            {
-               tail = tail.South;
-            }
-            else if (head.Y == tail.Y && (head.X - 2) == tail.X)
-            {
-               tail = tail.East;
-            }
-            else if (head.Y == tail.Y && (head.X + 2) == tail.X)
-            {
-               tail = tail.West;
-            }
-            else if (!head.IsAdjacentTo(tail, includeDiagonals: true))
-            {
-               if (tail.X < head.X && tail.Y < head.Y)
-               {
-                  tail = tail.North.East;
-               }
-               else if (tail.X < head.X && tail.Y > head.Y)
-               {
-                  tail = tail.South.East;
-               }
-               else if (tail.X > head.X && tail.Y < head.Y)
-               {
-                  tail = tail.North.West;
-               }
-               else if (tail.X > head.X && tail.Y > head.Y)
-               {
-                  tail = tail.South.West;
-               }
-            }
-            _ = tailPositions.Add(tail);
-         }
+         case 'U':
+            positions[0] = positions[0].North;
+            break;
+         case 'D':
+            positions[0] = positions[0].South;
+            break;
+         case 'L':
+            positions[0] = positions[0].West;
+            break;
+         case 'R':
+            positions[0] = positions[0].East;
+            break;
+         default:
+            break;
       }
 
-      return tailPositions.Count.ToString(System.Globalization.CultureInfo.CurrentCulture);
+      for (var index = 1; index < positions.Length; ++index)
+      {
+         if (positions[index - 1].X == positions[index].X && (positions[index - 1].Y - 2) == positions[index].Y)
+         {
+            positions[index] = positions[index].North;
+         }
+         else if (positions[index - 1].X == positions[index].X && (positions[index - 1].Y + 2) == positions[index].Y)
+         {
+            positions[index] = positions[index].South;
+         }
+         else if (positions[index - 1].Y == positions[index].Y && (positions[index - 1].X - 2) == positions[index].X)
+         {
+            positions[index] = positions[index].East;
+         }
+         else if (positions[index - 1].Y == positions[index].Y && (positions[index - 1].X + 2) == positions[index].X)
+         {
+            positions[index] = positions[index].West;
+         }
+         else if (!positions[index - 1].IsAdjacentTo(positions[index], includeDiagonals: true))
+         {
+            if (positions[index].X < positions[index - 1].X && positions[index].Y < positions[index - 1].Y)
+            {
+               positions[index] = positions[index].North.East;
+            }
+            else if (positions[index].X < positions[index - 1].X && positions[index].Y > positions[index - 1].Y)
+            {
+               positions[index] = positions[index].South.East;
+            }
+            else if (positions[index].X > positions[index - 1].X && positions[index].Y < positions[index - 1].Y)
+            {
+               positions[index] = positions[index].North.West;
+            }
+            else if (positions[index].X > positions[index - 1].X && positions[index].Y > positions[index - 1].Y)
+            {
+               positions[index] = positions[index].South.West;
+            }
+         }
+      }
    }
 
-   public override string SolvePartTwo()
+   private static int MoveRopeWithKnots(int knotCount, Tuple<char, int>[] instructions)
    {
-      var positions = Enumerable.Repeat(new Position.Position(), 10).ToArray();
+      var positions = Enumerable.Repeat(new Position(), knotCount).ToArray();
 
-      var tailPositions = new HashSet<Position.Position>
+      var tailPositions = new HashSet<Position>
       {
          positions.Last()
       };
 
-      foreach (var instruction in this.ParsedInput)
+      foreach (var instruction in instructions)
       {
          for (var count = 0; count < instruction.Item2; ++count)
          {
-            switch (instruction.Item1)
-            {
-               case 'U':
-                  positions[0] = positions[0].North;
-                  break;
-               case 'D':
-                  positions[0] = positions[0].South;
-                  break;
-               case 'L':
-                  positions[0] = positions[0].West;
-                  break;
-               case 'R':
-                  positions[0] = positions[0].East;
-                  break;
-               default:
-                  break;
-            }
-
-            for (var index = 1; index < positions.Length; ++index)
-            {
-               if (positions[index - 1].X == positions[index].X && (positions[index - 1].Y - 2) == positions[index].Y)
-               {
-                  positions[index] = positions[index].North;
-               }
-               else if (positions[index - 1].X == positions[index].X && (positions[index - 1].Y + 2) == positions[index].Y)
-               {
-                  positions[index] = positions[index].South;
-               }
-               else if (positions[index - 1].Y == positions[index].Y && (positions[index - 1].X - 2) == positions[index].X)
-               {
-                  positions[index] = positions[index].East;
-               }
-               else if (positions[index - 1].Y == positions[index].Y && (positions[index - 1].X + 2) == positions[index].X)
-               {
-                  positions[index] = positions[index].West;
-               }
-               else if (!positions[index - 1].IsAdjacentTo(positions[index], includeDiagonals: true))
-               {
-                  if (positions[index].X < positions[index - 1].X && positions[index].Y < positions[index - 1].Y)
-                  {
-                     positions[index] = positions[index].North.East;
-                  }
-                  else if (positions[index].X < positions[index - 1].X && positions[index].Y > positions[index - 1].Y)
-                  {
-                     positions[index] = positions[index].South.East;
-                  }
-                  else if (positions[index].X > positions[index - 1].X && positions[index].Y < positions[index - 1].Y)
-                  {
-                     positions[index] = positions[index].North.West;
-                  }
-                  else if (positions[index].X > positions[index - 1].X && positions[index].Y > positions[index - 1].Y)
-                  {
-                     positions[index] = positions[index].South.West;
-                  }
-               }
-            }
-
+            MoveRope(positions, instruction.Item1);
             _ = tailPositions.Add(positions.Last());
          }
       }
 
-      return tailPositions.Count.ToString(System.Globalization.CultureInfo.CurrentCulture);
+      return tailPositions.Count;
    }
+
+   public override string SolvePartOne() => MoveRopeWithKnots(2, this.ParsedInput).ToString(System.Globalization.CultureInfo.CurrentCulture);
+
+   public override string SolvePartTwo() => MoveRopeWithKnots(10, this.ParsedInput).ToString(System.Globalization.CultureInfo.CurrentCulture);
 }
