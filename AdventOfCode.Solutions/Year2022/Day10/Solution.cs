4e119f88c0b78ed6140c6e9d2061abdae53778b3
Ganon11 <stark3@gmail.com>
2022-12-10 15:14:35 -0500

    day(10): legosjedi had a brilliant callback-based solution, so i stole it :)

--- AdventOfCode.Solutions/Year2022/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day10/Solution.cs
@@ -2,76 +2,60 @@ namespace AdventOfCode.Solutions.Year2022.Day10;
 
 using AdventOfCode.Position;
 
-internal sealed class Solution : SolutionBase<string[]>
+internal sealed class Solution : SolutionBase<IInstruction[]>
 {
    public Solution() : base(10, 2022, "Cathode-Ray Tube") { }
 
-   public override string[] ParseInput(string input) => input.SplitByNewline(shouldTrim: true);
-
-   public override string SolvePartOne()
-   {
-      var x = 1;
-      var isAdding = false;
-      var eip = 0;
-      var indices = new HashSet<int>()
+   public override IInstruction[] ParseInput(string input) => input.SplitByNewline(shouldTrim: true)
+      .Select(line =>
       {
-         20,
-         60,
-         100,
-         140,
-         180,
-         220
-      };
-      var total = 0;
-      for (var clock = 1; clock <= 220; ++clock)
-      {
-         if (indices.Contains(clock))
+         if (line.Equals("noop", StringComparison.OrdinalIgnoreCase))
          {
-            total += clock * x;
-         }
-
-         if (this.ParsedInput.Length <= eip)
-         {
-            Console.WriteLine($"X is {x}, clock is {clock}");
-            Console.WriteLine($"eip is {eip}, past last instruction");
-            break;
-         }
-         if (isAdding)
-         {
-            x += int.Parse(this.ParsedInput[eip++].Split(' ').Last(), System.Globalization.CultureInfo.CurrentCulture);
-            isAdding = false;
-         }
-         else if (this.ParsedInput[eip].Equals("noop", StringComparison.Ordinal))
-         {
-            ++eip;
+            return new Noop();
          }
          else
          {
-            isAdding = true;
+            var value = int.Parse(line.Split(' ').Last(), System.Globalization.CultureInfo.CurrentCulture);
+            return new Add(value) as IInstruction;
          }
+      }).ToArray();
+
+   public override string SolvePartOne()
+   {
+      var total = 0;
+      var program = new Program(p =>
+      {
+         if ((p.Cycle + 20) % 40 != 0)
+         {
+            return;
+         }
+
+         total += p.X * p.Cycle;
+      });
+
+      foreach (var instruction in this.ParsedInput)
+      {
+         instruction.Execute(program);
       }
 
       return total.ToString(System.Globalization.CultureInfo.CurrentCulture);
    }
 
-   private static string RenderCrtDisplay(HashSet<Position> litPixels, int rows, int cols)
+   private static string RenderCrtDisplay(Dictionary<Position, char> pixels, int rows, int cols)
    {
       var sb = new StringBuilder();
-      sb.AppendLine();
+      _ = sb.AppendLine();
       for (var row = 0; row < rows; ++row)
       {
-         for (var col = 1; col <= cols; ++col)
+         for (var col = 0; col < cols; ++col)
          {
-            if (litPixels.Contains(new Position(x: col, y: row)))
+            var key = new Position(x: col, y: row);
+            if (pixels.TryGetValue(key, out var value))
             {
-               sb.Append('#');
-            }
-            else
-            {
-               sb.Append('.');
+               _ = sb.Append(value);
             }
          }
-         sb.AppendLine();
+         _ = sb.AppendLine();
       }
 
       return sb.ToString();
@@ -79,40 +63,70 @@ internal sealed class Solution : SolutionBase<string[]>
 
    public override string SolvePartTwo()
    {
-      var isAdding = false;
-      var eip = 0;
-      var crtDisplay = new HashSet<Position>();
-      var sprite = new Position(x: 1, y: -1);
-      for (var clock = 1; clock <= 240; ++clock)
+      var crtDisplay = new Dictionary<Position, char>();
+      var program = new Program(p =>
       {
-         var pixelPosition = (clock % 40) - 1;
-         if (pixelPosition == 0)
+         var validPositions = Enumerable.Range(p.X - 1, 3);
+         var pixelPosition = (p.Cycle - 1) % 40;
+         var screenPosition = new Position(x: pixelPosition, y: p.Cycle / 40);
+         char newPixel;
+         if (validPositions.Contains(pixelPosition))
          {
-            sprite.Y += 1;
-         }
-
-         // sprite.X + 1 <= pixelPosition <= sprite.X - 1
-         if (pixelPosition >= sprite.X - 1 && pixelPosition <= sprite.X + 1)
-         {
-            crtDisplay.Add(new Position(x: pixelPosition, y: sprite.Y));
-         }
-
-         if (isAdding)
-         {
-            var deltaX = int.Parse(this.ParsedInput[eip++].Split(' ').Last(), System.Globalization.CultureInfo.CurrentCulture);
-            sprite.X += deltaX;
-            isAdding = false;
-         }
-         else if (this.ParsedInput[eip].Equals("noop", StringComparison.Ordinal))
-         {
-            ++eip;
+            newPixel = '#';
          }
          else
          {
-            isAdding = true;
+            newPixel = ' ';
          }
+         crtDisplay.Add(screenPosition, newPixel);
+      });
+
+      foreach (var instruction in this.ParsedInput)
+      {
+         instruction.Execute(program);
       }
 
       return RenderCrtDisplay(crtDisplay, 6, 40);
+   }
+}
+
+internal sealed class Program
+{
+   public int Cycle { get; set; }
+   public int X { get; set; } = 1;
+   private readonly Action<Program> callback;
+
+   public Program(Action<Program> callback) => this.callback = callback;
+
+   public void AdvanceBy(int count)
+   {
+      for (var index = 0; index < count; ++index)
+      {
+         ++this.Cycle;
+         this.callback(this);
+      }
+   }
+}
+
+internal interface IInstruction
+{
+   void Execute(Program p);
+}
+
+internal sealed class Noop : IInstruction
+{
+   public void Execute(Program p) => p.AdvanceBy(1);
+}
+
+internal sealed class Add : IInstruction
+{
+   public int Value { get; init; }
+
+   public Add(int value) => this.Value = value;
+
+   public void Execute(Program p)
+   {
+      p.AdvanceBy(2);
+      p.X += this.Value;
    }
 }
