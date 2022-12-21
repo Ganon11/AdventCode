3d925c353435ed41b7bf2530a9754c9ea966c536
Mike Stark <mstark@factset.com>
2022-12-21 13:45:10 -0500

    day(19): closer to a solution... kinda

--- AdventOfCode.Solutions/Year2022/Day19/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day19/Solution.cs
@@ -1,4 +1,7 @@
 namespace AdventOfCode.Solutions.Year2022.Day19;
+
+using AdventOfCode.Solutions.Year2022.Day16;
+
 internal sealed class Solution : SolutionBase<Blueprint[]>
 {
    public Solution() : base(19, 2022, "Not Enough Minerals", true) { }
@@ -7,7 +10,8 @@ internal sealed class Solution : SolutionBase<Blueprint[]>
       .Select(line => new Blueprint(line))
       .ToArray();
 
-   private static int TIME_LIMIT = 24;
+   private const int TIME_LIMIT = 24;
+
    private static long MaxGeodes(Blueprint blueprint)
    {
       var start = new GeodeState
@@ -22,63 +26,132 @@ internal sealed class Solution : SolutionBase<Blueprint[]>
          GeodeRobots = 0
       };
 
-      var frontier = new PriorityQueue<GeodeState, long>();
-      frontier.Enqueue(start, 0);
+      var frontier = new PriorityQueue<GeodeState, long>(new IntMaxCompare());
+      frontier.Enqueue(start, start.GeodeRobots);
 
-      var cameFrom = new Dictionary<GeodeState, GeodeState?>();
-      cameFrom[start] = null;
+      var cameFrom = new Dictionary<GeodeState, GeodeState?>
+      {
+         [start] = null
+      };
 
-      var costSoFar = new Dictionary<GeodeState, long>();
-      costSoFar[start] = 0;
+      var costSoFar = new Dictionary<GeodeState, long>
+      {
+         [start] = 0
+      };
 
       while (frontier.Count > 0)
       {
          var current = frontier.Dequeue();
-
-         // Build an ore robot?
-         // We have the resources to build an ore robot
-         if (current.OreCache >= blueprint.RobotCosts[Resource.Ore][Resource.Ore])
+         if (costSoFar[current] >= TIME_LIMIT)
          {
-            var newCost = costSoFar[current] + 1;
-            if (newCost <= TIME_LIMIT)
-            {
-               var newState = new GeodeState()
-               {
-                  OreCache = current.OreCache - blueprint.RobotCosts[Resource.Ore][Resource.Ore] + current.OreRobots,
-                  ClayCache = current.ClayCache + current.ClayRobots,
-                  ObsidianCache = current.ObsidianCache + current.ObsidianRobots,
-                  GeodeCache = current.GeodeCache + current.GeodeRobots,
-                  OreRobots = current.OreRobots + 1,
-                  ClayRobots = current.ClayRobots,
-                  ObsidianRobots = current.ObsidianRobots,
-                  GeodeRobots = current.GeodeRobots
-               };
-               if (!costSoFar.TryGetValue(newState, out var existingCost) || newCost < existingCost)
-               {
-                  costSoFar[newState] = newCost;
-                  frontier.Enqueue(newState, newCost);
-                  cameFrom[newState] = current;
-               }
-            }
-         }
-         // You require more ore!
-         else
-         {
-            var oreNeeded = blueprint.RobotCosts[Resource.Ore][Resource.Ore] - current.OreCache;
-
+            continue;
          }
 
-         // Build a clay robot?
-
-         // Build an obsidian robot?
+         var neighbors = new List<GeodeState>();
+         var newCost = costSoFar[current] + 1;
 
          // Build a geode robot?
+         // We have the resources to build a geod robot
+         if (current.OreCache >= blueprint.RobotCosts[Resource.Geode][Resource.Ore] && current.ObsidianCache >= blueprint.RobotCosts[Resource.Geode][Resource.Obsidian])
+         {
+            neighbors.Add(new GeodeState
+            {
+               OreCache = current.OreCache - blueprint.RobotCosts[Resource.Geode][Resource.Ore] + current.OreRobots,
+               ClayCache = current.ClayCache + current.ClayRobots,
+               ObsidianCache = current.ObsidianCache - blueprint.RobotCosts[Resource.Geode][Resource.Obsidian] + current.ObsidianRobots,
+               GeodeCache = current.GeodeCache + current.GeodeRobots,
+               OreRobots = current.OreRobots,
+               ClayRobots = current.ClayRobots,
+               ObsidianRobots = current.ObsidianRobots,
+               GeodeRobots = current.GeodeRobots + 1
+            });
+         }
+         else if (current.OreCache >= blueprint.RobotCosts[Resource.Obsidian][Resource.Ore] && current.ClayCache >= blueprint.RobotCosts[Resource.Obsidian][Resource.Clay])
+         {
+            neighbors.Add(new GeodeState
+            {
+               OreCache = current.OreCache - blueprint.RobotCosts[Resource.Obsidian][Resource.Ore] + current.OreRobots,
+               ClayCache = current.ClayCache - blueprint.RobotCosts[Resource.Obsidian][Resource.Clay] + current.ClayRobots,
+               ObsidianCache = current.ObsidianCache + current.ObsidianRobots,
+               GeodeCache = current.GeodeCache + current.GeodeRobots,
+               OreRobots = current.OreRobots,
+               ClayRobots = current.ClayRobots,
+               ObsidianRobots = current.ObsidianRobots + 1,
+               GeodeRobots = current.GeodeRobots
+            });
+         }
+         else if (current.OreCache >= blueprint.RobotCosts[Resource.Clay][Resource.Ore])
+         {
+            neighbors.Add(new GeodeState
+            {
+               OreCache = current.OreCache - blueprint.RobotCosts[Resource.Clay][Resource.Ore] + current.OreRobots,
+               ClayCache = current.ClayCache + current.ClayRobots,
+               ObsidianCache = current.ObsidianCache + current.ObsidianRobots,
+               GeodeCache = current.GeodeCache + current.GeodeRobots,
+               OreRobots = current.OreRobots,
+               ClayRobots = current.ClayRobots + 1,
+               ObsidianRobots = current.ObsidianRobots,
+               GeodeRobots = current.GeodeRobots
+            });
+         }
+         else if (current.OreCache >= blueprint.RobotCosts[Resource.Ore][Resource.Ore])
+         {
+            neighbors.Add(new GeodeState
+            {
+               OreCache = current.OreCache - blueprint.RobotCosts[Resource.Ore][Resource.Ore] + current.OreRobots,
+               ClayCache = current.ClayCache + current.ClayRobots,
+               ObsidianCache = current.ObsidianCache + current.ObsidianRobots,
+               GeodeCache = current.GeodeCache + current.GeodeRobots,
+               OreRobots = current.OreRobots + 1,
+               ClayRobots = current.ClayRobots,
+               ObsidianRobots = current.ObsidianRobots,
+               GeodeRobots = current.GeodeRobots
+            });
+         }
+
+         // If we can't build a robot, wait it out
+         if (!neighbors.Any())
+         {
+            neighbors.Add(new GeodeState
+            {
+               OreCache = current.OreCache + current.OreRobots,
+               ClayCache = current.ClayCache + current.ClayRobots,
+               ObsidianCache = current.ObsidianCache + current.ObsidianRobots,
+               GeodeCache = current.GeodeCache + current.GeodeRobots,
+               OreRobots = current.OreRobots,
+               ClayRobots = current.ClayRobots,
+               ObsidianRobots = current.ObsidianRobots,
+               GeodeRobots = current.GeodeRobots
+            });
+         }
+
+         // Add new states
+         foreach (var neighbor in neighbors)
+         {
+            if (!costSoFar.TryGetValue(neighbor, out var existingCost) || newCost < existingCost)
+            {
+               costSoFar[neighbor] = newCost;
+               var priority = newCost + current.GeodeRobots;
+               frontier.Enqueue(neighbor, priority);
+               cameFrom[neighbor] = current;
+            }
+         }
       }
+
+      return costSoFar.Keys.Max(state => state.GeodeCache);
    }
 
    public override string SolvePartOne()
    {
-      return "";
+      long sum = 0;
+      foreach (var blueprint in this.ParsedInput)
+      {
+         long maxGeodes = MaxGeodes(blueprint);
+         long qualityLevel = blueprint.Id * maxGeodes;
+         sum += qualityLevel;
+      }
+
+      return sum.ToString(System.Globalization.CultureInfo.CurrentCulture);
    }
 
    public override string SolvePartTwo()
@@ -104,7 +177,7 @@ internal class Blueprint
    {
       var tokens = line.Split(' ');
 
-      this.Id = int.Parse(tokens[1], System.Globalization.CultureInfo.CurrentCulture);
+      this.Id = int.Parse(tokens[1].Trim(':'), System.Globalization.CultureInfo.CurrentCulture);
 
       var oreRobotCost = new Dictionary<Resource, long>
       {
@@ -157,8 +230,22 @@ internal class GeodeState : IEquatable<GeodeState>
       return this.OreRobots == other.OreRobots &&
          this.ClayRobots == other.ClayRobots &&
          this.ObsidianRobots == other.ObsidianRobots &&
-         this.GeodeRobots == other.GeodeRobots;
+         this.GeodeRobots == other.GeodeRobots &&
+         this.OreCache == other.OreCache &&
+         this.ClayCache == other.ClayCache &&
+         this.ObsidianCache == other.ObsidianCache &&
+         this.GeodeCache == other.GeodeCache;
    }
 
    public override bool Equals(object? obj) => this.Equals(obj as GeodeState);
+
+   public override int GetHashCode() => new Tuple<long, long, long, long, long, long, Tuple<long, long>>(
+      this.OreRobots,
+      this.ClayRobots,
+      this.ObsidianRobots,
+      this.GeodeRobots,
+      this.OreCache,
+      this.ClayCache,
+      new(this.ObsidianCache, this.GeodeCache)
+      ).GetHashCode();
 }
