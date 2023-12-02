fca271299dc953fb327e30a7eb20fae718c90a7d
Ganon11 <stark3@gmail.com>
2023-12-01 23:11:27 -0500

    feat: add a bunch of new code to position

--- 2023/helpers/position.h
+++ 2023/helpers/position.h
@@ -5,11 +5,22 @@
 #include <vector>
 
 namespace advent_of_code {
-struct Position
+class Position
 {
+public:
   Position() = default;
   Position(const long long x, const long long y, const long long z = 0, const long long a = 0);
+  Position(const std::string& line);
+
+  ~Position() = default;
   Position(const Position& p) = default;
+  Position& operator=(const Position& p) = default;
+
+  long long x() const { return m_x; }
+  long long y() const { return m_y; }
+  long long z() const { return m_z; }
+  long long a() const { return m_a; }
+
   bool operator<(const Position& other) const;
   bool operator>(const Position& other) const;
   bool operator==(const Position& other) const;
@@ -20,10 +31,14 @@ struct Position
   Position south() const;
   Position east() const;
   Position west() const;
-  std::vector<Position> get_adjacent_positions() const;
   Position above() const;
   Position below() const;
+  Position past() const;
+  Position future() const;
+
+  std::vector<Position> get_adjacent_positions() const;
   std::vector<Position> get_adjacent_positions_3D() const;
+  std::vector<Position> get_adjacent_positions_4D() const;
 
   size_t distance_to(const Position& other) const;
 
@@ -33,10 +48,14 @@ struct Position
     return out;
   }
 
+private:
   long long m_x;
   long long m_y;
   long long m_z;
   long long m_a; // THE FOURTH DIMENSION WOOOOAH
+
+public:
+  static std::vector<Position> getPositionsInLine(const Position& p1, const Position& p2);
 };
 
 #ifdef max
@@ -59,7 +78,7 @@ template <> struct hash<advent_of_code::Position>
   typedef advent_of_code::Position argument_type;
   typedef std::size_t result_type;
   std::size_t operator()(const advent_of_code::Position& p) const noexcept {
-    return std::hash<long long>()(p.m_x ^ (p.m_y << 4) ^ (p.m_z << 8));
+    return std::hash<long long>()(p.x() ^ (p.y() << 4) ^ (p.z() << 8) ^ (p.a() << 12));
   }
 };
 }
