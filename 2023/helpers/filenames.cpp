9571b0643a9649eb40ca4b1c1c80723acf47f812
Ganon11 <stark3@gmail.com>
2023-12-01 23:28:51 -0500

    refactor: filenames, sample not adding value

--- 2023/helpers/filenames.cpp
+++
@@ -1,7 +0,0 @@
-#include "filenames.h"
-#include "sample.h"
-
-const char* advent_of_code::filenames::get_filename()
-{
-  return advent_of_code::sample::should_use_sample() ? SAMPLE_FILENAME : INPUT_FILENAME;
-}
--- 2023/helpers/input_handler.h
+++ 2023/helpers/input_handler.h
@@ -8,16 +8,12 @@ namespace advent_of_code {
 class InputHandler
 {
 public:
-  InputHandler(const int argc, char** argv);
   InputHandler(const std::string& filename);
 
   std::string read_single_line() const;
   std::vector<std::string> read_all_lines() const;
 
-  bool get_argument(const std::string& flag, std::string& value) const;
-
 private:
   std::string m_filename;
-  std::map<std::string, std::string> m_arguments;
 };
 }
