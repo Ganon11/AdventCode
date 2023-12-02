68b6cab25b2b6671230000a15ebd1a4ddb5ff04a
Ganon11 <stark3@gmail.com>
2023-12-01 23:28:51 -0500

    refactor: filenames, sample not adding value

--- 2023/helpers/input_handler.cpp
+++ 2023/helpers/input_handler.cpp
@@ -3,36 +3,7 @@
 #include <sstream>
 #include <string>
 
-#include "filenames.h"
 #include "input_handler.h"
-#include "sample.h"
-
-advent_of_code::InputHandler::InputHandler(const int argc, char** argv)
-{
-  bool filename_set = false;
-  for (int argv_index = 1; argv_index < argc; ++argv_index)
-  {
-    if (0 == strcmp("-use-sample", argv[argv_index]))
-    {
-      sample::set_use_sample(true);
-    }
-    else if (0 == strcmp("-filename", argv[argv_index]) && argv_index + 1 < argc)
-    {
-      m_filename = std::string(argv[argv_index + 1]);
-      ++argv_index;
-      filename_set = true;
-    }
-    else if (argv_index + 1 < argc)
-    {
-      m_arguments[argv[argv_index]] = argv[argv_index + 1];
-    }
-  }
-
-  if (!filename_set)
-  {
-    m_filename = filenames::get_filename();
-  }
-}
 
 advent_of_code::InputHandler::InputHandler(const std::string& filename) : m_filename{ filename }
 {}
@@ -61,16 +32,4 @@ std::vector<std::string> advent_of_code::InputHandler::read_all_lines() const
   }
 
   return lines;
-}
-
-bool advent_of_code::InputHandler::get_argument(const std::string& flag, std::string& value) const
-{
-  auto arg{ m_arguments.find(flag) };
-  if (m_arguments.end() == arg)
-  {
-    return false;
-  }
-
-  value = arg->second;
-  return true;
 }
--- 2023/helpers/sample.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace advent_of_code {
-namespace sample {
-bool should_use_sample();
-void set_use_sample(const bool should_use_sample);
-}
-}
