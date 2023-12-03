f5da18b448f9bf3b5a74ed332dadb78529a4c546
Ganon11 <stark3@gmail.com>
2023-12-03 17:14:14 -0500

    feat: day 2 solution

---
+++ 2023/day2/day2.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "input_handler.h"
+#include "cxxopts.hpp"
+
+namespace {
+static short PART = 1;
+static std::map<std::string, unsigned short> LIMITS =
+{
+  { "red", 12 },
+  { "green", 13 },
+  { "blue", 14 }
+};
+}
+
+int main(int argc, char* argv[])
+{
+  cxxopts::Options options("d1", "Day 1 of Advent of Code");
+
+  options.add_options()
+    ("f,filename", "Input Filename", cxxopts::value<std::string>())
+    ("p,part", "Part 1 or 2", cxxopts::value<short>()->default_value("1"))
+  ;
+
+  auto result = options.parse(argc, argv);
+  if (!result.count("filename"))
+  {
+    return -1;
+  }
+
+  if (result.count("part"))
+  {
+    ::PART = result["part"].as<short>();
+  }
+
+  advent_of_code::InputHandler input{ result["filename"].as<std::string>() };
+  unsigned int valid_game_sum{0};
+  unsigned long long power_set_sum{0};
+
+  std::vector<std::string> games = input.read_all_lines();
+
+  for (size_t index = 0; index < games.size(); ++index)
+  {
+    std::map<std::string, unsigned short> maximums = {
+      { "red", 0 },
+      { "green", 0 },
+      { "blue", 0 }
+    };
+    std::vector<std::string> tokens = advent_of_code::tokenize(games[index], ": ");
+
+    const std::string& pulls_string = tokens[1];
+    std::vector<std::string> pulls = advent_of_code::tokenize(pulls_string, "; ");
+    bool valid_game = true;
+    for (const auto& pull : pulls)
+    {
+      std::vector<std::string> colors = advent_of_code::tokenize(pull, ", ");
+      for (const auto& color : colors)
+      {
+        std::vector<std::string> info = advent_of_code::tokenize(color, " ");
+        unsigned short count = std::stoi(info[0]);
+        std::string c = info[1];
+        maximums[c] = std::max(maximums[c], count);
+
+        if (count > LIMITS[c])
+        {
+          valid_game = false;
+        }
+      }
+    }
+
+    power_set_sum += maximums["red"] * maximums["green"] * maximums["blue"];
+    if (valid_game)
+    {
+      valid_game_sum += index + 1;
+    }
+  }
+
+  std::cout << "Valid puzzle sum: " << valid_game_sum << std::endl;
+  std::cout << "Power set sum: " << power_set_sum << std::endl;
+  return 0;
+}
--- 2023/helpers/input_handler.cpp
+++ 2023/helpers/input_handler.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 
+#include <iostream>
+
 #include "input_handler.h"
 
 advent_of_code::InputHandler::InputHandler(const std::string& filename) : m_filename{ filename }
@@ -32,4 +34,58 @@ std::vector<std::string> advent_of_code::InputHandler::read_all_lines() const
   }
 
   return lines;
+}
+
+std::vector<std::string> advent_of_code::tokenize(const std::string& line, const char separator = ' ')
+{
+  std::vector<std::string> tokens;
+
+  const char* str = &line[0];
+  do
+  {
+    const char* begin = str;
+
+    while (*str != separator && *str)
+    {
+      ++str;
+    }
+
+    tokens.push_back(std::string{ begin, str });
+  } while ('\0' != *str++);
+
+  return tokens;
+}
+
+std::vector<std::string> advent_of_code::tokenize(const std::string& line, const std::string& separator)
+{
+  //std::cout << "Tokenizing string \"" << line << "\" by delimiter \"" << separator << "\"" << std::endl;
+  std::vector<std::string> tokens;
+  size_t index = 0;
+
+  while (index < line.size())
+  {
+    std::string temp;
+    size_t position = line.find(separator, index);
+    if (position == std::string::npos)
+    {
+      //std::cout << "\tDelimiter not found, taking the rest of the string." << std::endl;
+      temp = line.substr(index);
+      index = line.size();
+    }
+    else
+    {
+      //std::cout << "\tDelimiter found at " << position << ", taking substring." << std::endl;
+      temp = line.substr(index, position - index);
+      index = position + separator.size();
+      //std::cout << "\tIndex updated to " << index << std::endl;
+    }
+
+    if (!temp.empty())
+    {
+      //std::cout << "\tAdding token \"" << temp << "\"" << std::endl;
+      tokens.push_back(temp);
+    }
+  }
+
+  return tokens;
 }
