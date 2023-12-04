0cb602c66e0d6e12424cf8b9705485c879012307
Michael Stark <mstark@factset.com>
2023-12-04 16:53:44 -0500

    refactor: cleanup

--- 2023/day3/day3.cpp
+++ 2023/day3/day3.cpp
@@ -8,168 +8,163 @@
 #include "position.h"
 #include "schematic.h"
 
-unsigned long long part_1(const std::map<advent_of_code::Position, SchematicSquare>& schematic)
+typedef std::map<advent_of_code::Position, SchematicSquare> Schematic;
+typedef Schematic::value_type SchematicValue;
+
+unsigned long long part_1(const Schematic& schematic)
 {
   unsigned long long total{0};
+  auto max_column = std::max_element(schematic.begin(), schematic.end(), [](const SchematicValue& a, const SchematicValue& b){ return a.first.x() < b.first.x(); })->first.x() + 1;
   std::set<advent_of_code::Position> numbers_accounted_for;
   for (const auto& [k, v] : schematic)
   {
-    if (v.type == POSITION_TYPE::SYMBOL)
+    if (v.type != POSITION_TYPE::SYMBOL)
     {
-      //std::cout << "Looking at symbol " << v.character << " at " << v.position << std::endl;
-      for (const auto& adj : k.get_adjacent_positions(true))
+      continue;
+    }
+
+    for (const auto& adj : k.get_adjacent_positions(true))
+    {
+      if (schematic.find(adj) == schematic.end())
       {
-        if (schematic.find(adj) == schematic.end())
-        {
-          continue;
-        }
-
-        if (schematic.at(adj).type != POSITION_TYPE::NUMBER)
-        {
-          continue;
-        }
-
-        if (numbers_accounted_for.find(adj) != numbers_accounted_for.end())
-        {
-          continue;
-        }
-
-        //std::cout << "Unaccounted number found at adjacent position " << adj << std::endl;
-
-        // Find first column with number
-        long long start = -1;
-        for (long long index = adj.x(); index >= 0; --index)
-        {
-          advent_of_code::Position p{ index, adj.y() };
-          //std::cout << "Is " << p << " a number?" << std::endl;
-          if (schematic.at(p).type != POSITION_TYPE::NUMBER)
-          {
-            start = index + 1;
-            break;
-          }
-        }
-
-        if (start == -1)
-        {
-          start = 0;
-        }
-
-        // Find last column with number
-        long long end;
-        for (long long index = adj.x(); index < 1000; ++index)
-        {
-          advent_of_code::Position p{ index, adj.y() };
-          if (schematic.find(p) == schematic.end() ||
-            schematic.at(p).type != POSITION_TYPE::NUMBER)
-          {
-            end = index - 1;
-            break;
-          }
-        }
-
-        //std::cout << "Formed number at y=" << adj.y() << " from columns " << start << " through " << end << std::endl;
-
-        // Build number
-        unsigned long long subtotal{0};
-        for (long long index = start; index <= end; ++index)
-        {
-          advent_of_code::Position p{ index, adj.y() };
-          numbers_accounted_for.insert(p);
-          subtotal *= 10;
-          subtotal += schematic.at(p).character - '0';
-        }
-
-        //std::cout << "Adding part number " << subtotal << std::endl;
-        total += subtotal;
+        continue;
       }
+
+      if (schematic.at(adj).type != POSITION_TYPE::NUMBER)
+      {
+        continue;
+      }
+
+      if (numbers_accounted_for.contains(adj))
+      {
+        continue;
+      }
+
+      // Find first column with number
+      long long start = -1;
+      for (long long index = adj.x(); index >= 0; --index)
+      {
+        advent_of_code::Position p{ index, adj.y() };
+        if (schematic.at(p).type != POSITION_TYPE::NUMBER)
+        {
+          start = index + 1;
+          break;
+        }
+      }
+
+      if (start == -1)
+      {
+        start = 0;
+      }
+
+      // Find last column with number
+      long long end;
+      for (long long index = adj.x(); index <= max_column; ++index)
+      {
+        advent_of_code::Position p{ index, adj.y() };
+        if (!schematic.contains(p) ||
+          schematic.at(p).type != POSITION_TYPE::NUMBER)
+        {
+          end = index - 1;
+          break;
+        }
+      }
+
+      // Build number
+      unsigned long long subtotal{0};
+      for (long long index = start; index <= end; ++index)
+      {
+        advent_of_code::Position p{ index, adj.y() };
+        numbers_accounted_for.insert(p);
+        subtotal *= 10;
+        subtotal += schematic.at(p).character - '0';
+      }
+
+      total += subtotal;
     }
   }
 
   return total;
 }
 
-unsigned long long part_2(const std::map<advent_of_code::Position, SchematicSquare>& schematic)
+unsigned long long part_2(const Schematic& schematic)
 {
   unsigned long long total{0};
+  auto max_column = std::max_element(schematic.begin(), schematic.end(), [](const SchematicValue& a, const SchematicValue& b){ return a.first.x() < b.first.x(); })->first.x() + 1;
   for (const auto& [k, v] : schematic)
   {
-    if (v.type == POSITION_TYPE::SYMBOL && v.character == '*')
+    if (v.character != '*')
     {
-      unsigned short number_count{0};
-      std::set<advent_of_code::Position> numbers_accounted_for;
-      unsigned long long product{1};
-      //std::cout << "Looking at symbol " << v.character << " at " << v.position << std::endl;
-      for (const auto& adj : k.get_adjacent_positions(true))
+      continue;
+    }
+
+    unsigned short number_count{0};
+    std::set<advent_of_code::Position> numbers_accounted_for;
+    unsigned long long product{1};
+    for (const auto& adj : k.get_adjacent_positions(true))
+    {
+      if (!schematic.contains(adj))
       {
-        if (schematic.find(adj) == schematic.end())
-        {
-          continue;
-        }
-
-        if (schematic.at(adj).type != POSITION_TYPE::NUMBER)
-        {
-          continue;
-        }
-
-        if (numbers_accounted_for.find(adj) != numbers_accounted_for.end())
-        {
-          continue;
-        }
-
-        //std::cout << "Unaccounted number found at adjacent position " << adj << std::endl;
-
-        // Find first column with number
-        long long start = -1;
-        for (long long index = adj.x(); index >= 0; --index)
-        {
-          advent_of_code::Position p{ index, adj.y() };
-          //std::cout << "Is " << p << " a number?" << std::endl;
-          if (schematic.at(p).type != POSITION_TYPE::NUMBER)
-          {
-            start = index + 1;
-            break;
-          }
-        }
-
-        if (start == -1)
-        {
-          start = 0;
-        }
-
-        // Find last column with number
-        long long end;
-        for (long long index = adj.x(); index < 1000; ++index)
-        {
-          advent_of_code::Position p{ index, adj.y() };
-          if (schematic.find(p) == schematic.end() ||
-            schematic.at(p).type != POSITION_TYPE::NUMBER)
-          {
-            end = index - 1;
-            break;
-          }
-        }
-
-        //std::cout << "Formed number at y=" << adj.y() << " from columns " << start << " through " << end << std::endl;
-
-        // Build number
-        unsigned long long subtotal{0};
-        for (long long index = start; index <= end; ++index)
-        {
-          advent_of_code::Position p{ index, adj.y() };
-          numbers_accounted_for.insert(p);
-          subtotal *= 10;
-          subtotal += schematic.at(p).character - '0';
-        }
-
-        //std::cout << "Adding part number " << subtotal << std::endl;
-        product *= subtotal;
-        ++number_count;
+        continue;
       }
 
-      if (number_count == 2)
+      if (schematic.at(adj).type != POSITION_TYPE::NUMBER)
       {
-        total += product;
+        continue;
       }
+
+      if (numbers_accounted_for.contains(adj))
+      {
+        continue;
+      }
+
+      // Find first column with number
+      long long start = -1;
+      for (long long index = adj.x(); index >= 0; --index)
+      {
+        advent_of_code::Position p{ index, adj.y() };
+        if (schematic.at(p).type != POSITION_TYPE::NUMBER)
+        {
+          start = index + 1;
+          break;
+        }
+      }
+
+      if (start == -1)
+      {
+        start = 0;
+      }
+
+      // Find last column with number
+      long long end;
+      for (long long index = adj.x(); index <= max_column; ++index)
+      {
+        advent_of_code::Position p{ index, adj.y() };
+        if (!schematic.contains(p) ||
+          schematic.at(p).type != POSITION_TYPE::NUMBER)
+        {
+          end = index - 1;
+          break;
+        }
+      }
+
+      // Build number
+      unsigned long long subtotal{0};
+      for (long long index = start; index <= end; ++index)
+      {
+        advent_of_code::Position p{ index, adj.y() };
+        numbers_accounted_for.insert(p);
+        subtotal *= 10;
+        subtotal += schematic.at(p).character - '0';
+      }
+
+      product *= subtotal;
+      ++number_count;
+    }
+
+    if (number_count == 2)
+    {
+      total += product;
     }
   }
 
@@ -193,7 +188,7 @@ int main(int argc, char* argv[])
   advent_of_code::InputHandler input{ result["filename"].as<std::string>() };
 
   std::vector<std::string> lines = input.read_all_lines();
-  std::map<advent_of_code::Position, SchematicSquare> schematic;
+  Schematic schematic;
   for (size_t row = 0; row < lines.size(); ++row)
   {
     for (size_t col = 0; col < lines[row].size(); ++col)
