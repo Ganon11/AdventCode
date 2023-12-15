0483396883a3a871207e34a39aad911f9ff1a892
Michael Stark <mstark@factset.com>
2023-12-15 15:33:47 -0500

    chore: day 12, 13, 14 scaffolding

---
+++ 2023/day14/day14.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "cxxopts.hpp"
+#include "input_handler.h"
+
+int main(int argc, char* argv[])
+{
+  cxxopts::Options options("d14", "Day 14 of Advent of Code");
+  options.add_options()
+    ("f,filename", "Input Filename", cxxopts::value<std::string>())
+    ("h,help", "Print usage")
+  ;
+
+  auto result = options.parse(argc, argv);
+  if (result.count("help"))
+  {
+    std::cout << options.help() << std::endl;
+    return 0;
+  }
+
+  if (!result.count("filename"))
+  {
+    std::cout << "ERROR: Missing required argument: <filename>" << std::endl;
+    std::cout << options.help() << std::endl;
+    return -1;
+  }
+
+  advent_of_code::InputHandler input{ result["filename"].as<std::string>() };
+  return 0;
+}
