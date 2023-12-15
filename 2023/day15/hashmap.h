e619bbee09febe7df741d370400603a494fb57a0
Ganon11 <stark3@gmail.com>
2023-12-15 16:18:54 -0500

    refactor: make insertion and removal private

--- 2023/day15/hashmap.h
+++ 2023/day15/hashmap.h
@@ -22,14 +22,15 @@ public:
   HASHMAP(const std::function<unsigned char(const std::string&)> hash_func = hash);
 
   void process_command(const std::string& command);
-  void insert(const unsigned int index, const Lens& lens);
-  void remove(const unsigned int index, const std::string& label);
 
   unsigned long long focusing_power() const;
 
   std::string to_string() const;
 
 private:
+  void insert(const unsigned int index, const Lens& lens);
+  void remove(const unsigned int index, const std::string& label);
+
   std::map<unsigned int, std::vector<Lens>> m_boxes;
   std::function<unsigned char(const std::string&)> m_hash;
 };
