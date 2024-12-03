e071c913ea5492b85c4725037fca568e07e9e1fe
Ganon11 <stark3@gmail.com>
2024-12-03 11:49:05 -0500

    chore: reformatting

--- 2024/Day03/d3.rb
+++ 2024/Day03/d3.rb
@@ -1,13 +1,5 @@
 require 'optparse'
 
-DO_FORMAT    = /^do\(\)/
-DONT_FORMAT  = /^don't\(\)/
-MUL_FORMAT   = /^mul\((\d{1,3}),(\d{1,3})\)/
-def find_mul(memory, do_dont = false)
-
-  sum
-end
-
 options = {
   :part => 1,
   :filename => 'input.txt'
@@ -20,28 +12,28 @@ OptionParser.new do |opts|
 end.parse!(into: options)
 
 memory_lines = IO.readlines(options[:filename]).map(&:strip)
+
 sum = 0
 enabled = true
+DO_FORMAT    = /^do\(\)/
+DONT_FORMAT  = /^don't\(\)/
+MUL_FORMAT   = /^mul\((\d{1,3}),(\d{1,3})\)/
+
 memory_lines.each do |memory|
   (0..memory.length - 1).each do |start|
     end_index = [start + 11, memory.length - 1].min
     substr = memory[start..end_index]
-    #puts "Checking substr \"#{substr}\""
     if substr.match(DO_FORMAT)
-      #puts "do() found"
       enabled = true
     elsif substr.match(DONT_FORMAT)
-      #puts "don't() found"
       enabled = false
     elsif m = substr.match(MUL_FORMAT)
-      #puts "mult found: #{substr}"
       if options[:part] == 2 && !enabled
-        #puts "Ignoring mult..."
         next
       end
-      #puts "performing mult"
       sum += m.captures[0].to_i * m.captures[1].to_i
     end
   end
 end
+
 puts "Sum: #{sum}"
