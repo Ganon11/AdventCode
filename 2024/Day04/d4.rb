9bfbcc866cfad51d0d0a8e825d5a2a506ff5d348
Ganon11 <stark3@gmail.com>
2024-12-04 14:53:13 -0500

    chore: move point class into its own directory

--- 2024/Day04/d4.rb
+++ 2024/Day04/d4.rb
@@ -1,25 +1,5 @@
 require 'optparse'
-
-class Point
-  attr_reader :x, :y
-
-  def initialize(x, y)
-    @x = x
-    @y = y
-  end
-
-  def eql?(other)
-    self.x == other.x && self.y == other.y
-  end
-
-  def hash
-    [x, y].hash
-  end
-
-  def to_s
-    "(#{x}, #{y})"
-  end
-end
+require '../point/point.rb'
 
 def find_max_row(puzzle)
   puzzle.keys.max_by{ |key, value| key.y }.y
