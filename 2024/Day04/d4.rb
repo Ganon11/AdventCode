ea5a51f4dcd00f9f3914c1ce2c9619c446566c8a
Ganon11 <stark3@gmail.com>
2024-12-04 15:59:36 -0500

    chore: add type signatures to day 4

--- 2024/Day04/d4.rb
+++ 2024/Day04/d4.rb
@@ -1,14 +1,24 @@
 require 'optparse'
 require 'point'
 
+# typed: true
+extend T::Sig
+
+sig {params(puzzle: T::Hash[Point::Point, String]).returns(Integer)}
 def find_max_row(puzzle)
-  puzzle.keys.max_by{ |key, value| key.y }.y
+  max_row_point = puzzle.keys.max_by{ |key, value| key.y }
+  max_row_point.y if max_row_point
+  -1
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String]).returns(Integer)}
 def find_max_col(puzzle)
-  puzzle.keys.max_by{ |key, value| key.x }.x
+  max_col_point = puzzle.keys.max_by{ |key, value| key.x }
+  max_col_point.x if max_col_point
+  -1
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], x: Point::Point, m: Point::Point, a: Point::Point, s: Point::Point).returns(T::Boolean)}
 def search(puzzle, x, m, a, s)
   if [x, m, a, s].any? { |point| !puzzle.key?(point) }
     false
@@ -17,38 +27,47 @@ def search(puzzle, x, m, a, s)
   puzzle[x] == 'X' && puzzle[m] == 'M' && puzzle[a] == 'A' && puzzle[s] == 'S'
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_right(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x + 1, p.y), Point::Point.new(p.x + 2, p.y), Point::Point.new(p.x + 3, p.y))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_left(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x - 1, p.y), Point::Point.new(p.x - 2, p.y), Point::Point.new(p.x - 3, p.y))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_down(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x, p.y + 1), Point::Point.new(p.x, p.y + 2), Point::Point.new(p.x, p.y + 3))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_up(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x, p.y - 1), Point::Point.new(p.x, p.y - 2), Point::Point.new(p.x, p.y - 3))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_dr(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x + 1, p.y + 1), Point::Point.new(p.x + 2, p.y + 2), Point::Point.new(p.x + 3, p.y + 3))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_dl(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x - 1, p.y + 1), Point::Point.new(p.x - 2, p.y + 2), Point::Point.new(p.x - 3, p.y + 3))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_ur(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x + 1, p.y - 1), Point::Point.new(p.x + 2, p.y - 2), Point::Point.new(p.x + 3, p.y - 3))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_ul(puzzle, p)
   search(puzzle, p, Point::Point.new(p.x - 1, p.y - 1), Point::Point.new(p.x - 2, p.y - 2), Point::Point.new(p.x - 3, p.y - 3))
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], m_1: Point::Point, m_2: Point::Point, a: Point::Point, s_1: Point::Point, s_2: Point::Point).returns(T::Boolean)}
 def search_x_mas(puzzle, m_1, m_2, a, s_1, s_2)
   if [m_1, m_2, a, s_1, s_2].any? { |point| !puzzle.key?(point) }
     false
@@ -57,6 +76,7 @@ def search_x_mas(puzzle, m_1, m_2, a, s_1, s_2)
   puzzle[m_1] == 'M' && puzzle[m_2] == 'M' && puzzle[a] == 'A' && puzzle[s_1] == 'S' && puzzle[s_2] == 'S'
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_x_mas_msams(puzzle, p)
   m_1 = p
   m_2 = Point::Point.new(p.x, p.y + 2)
@@ -66,6 +86,7 @@ def search_x_mas_msams(puzzle, p)
   search_x_mas(puzzle, m_1, m_2, a, s_1, s_2)
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_x_mas_mmass(puzzle, p)
   m_1 = p
   m_2 = Point::Point.new(p.x + 2, p.y)
@@ -75,6 +96,7 @@ def search_x_mas_mmass(puzzle, p)
   search_x_mas(puzzle, m_1, m_2, a, s_1, s_2)
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_x_mas_smasm(puzzle, p)
   s_1 = p
   s_2 = Point::Point.new(p.x, p.y + 2)
@@ -84,6 +106,7 @@ def search_x_mas_smasm(puzzle, p)
   search_x_mas(puzzle, m_1, m_2, a, s_1, s_2)
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String], p: Point::Point).returns(T::Boolean)}
 def search_x_mas_ssamm(puzzle, p)
   s_1 = p
   s_2 = Point::Point.new(p.x + 2, p.y)
@@ -93,6 +116,7 @@ def search_x_mas_ssamm(puzzle, p)
   search_x_mas(puzzle, m_1, m_2, a, s_1, s_2)
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String]).returns(Integer)}
 def find_xmas(puzzle)
   max_row = find_max_row(puzzle)
   max_col = find_max_col(puzzle)
@@ -115,6 +139,7 @@ def find_xmas(puzzle)
   sum
 end
 
+sig {params(puzzle: T::Hash[Point::Point, String]).returns(Integer)}
 def find_x_mas(puzzle)
   max_row = find_max_row(puzzle)
   max_col = find_max_col(puzzle)
