5df38aa986729b7a8e61679207bba6b3664c5801
Ganon11 <stark3@gmail.com>
2024-12-11 13:21:41 -0500

    feat: day 11 part 2 solution
    Memoization to the rescue, as usual.

--- 2024/Day11/d11.rb
+++ 2024/Day11/d11.rb
@@ -1,53 +1,84 @@
-require 'linked_list'
 require 'optparse'
 require 'sorbet-runtime'
 
 # typed: true
 extend T::Sig
 
-sig {params(list: LinkedList::LinkedList).returns(LinkedList::LinkedList)}
-def blink(list)
-  current = list.head
-  new_list = LinkedList::LinkedList.new
-  while current
-    if current.value == 0
-      stone_digits = 1
-    else
-      stone_digits = Math.log10(current.value).to_i + 1
-    end
-
-    if current.value == 0 # Rule 1: If the stone is engraved with 0, change value to 1
-      new_list.append(1)
-    elsif stone_digits % 2 == 0 # Rule 2: If the stone has an even number of digits, split into 2 stones
-      power_of_ten = (10 ** (stone_digits / 2))
-      new_list.append(current.value / power_of_ten)
-      new_list.append(current.value % power_of_ten)
-    else # Rule 3: Multiply the stone's value by 2024
-      new_list.append(current.value * 2024)
-    end
-
-    current = current.next
+sig {params(stone: Integer).returns(T::Array[Integer])}
+def transform_stone(stone)
+  if stone == 0
+    stone_digits = 1
+  else
+    stone_digits = Math.log10(stone).to_i + 1
   end
 
-  return new_list
+  if stone == 0 # Rule 1: If the stone is engraved with 0, change value to 1
+    return [1]
+  elsif stone_digits % 2 == 0 # Rule 2: If the stone has an even number of digits, split into 2 stones
+    power_of_ten = (10 ** (stone_digits / 2))
+    return [(stone / power_of_ten).to_i, (stone % power_of_ten).to_i]
+  else # Rule 3: Multiply the stone's value by 2024
+    return [stone * 2024]
+  end
+end
+
+class BlinkMemo
+  extend T::Sig
+
+  sig {returns(Integer)}
+  attr_reader :stone
+
+  sig {returns(Integer)}
+  attr_reader :depth
+
+  sig {params(stone: Integer, depth: Integer).void}
+  def initialize(stone, depth)
+    @stone = stone
+    @depth = depth
+  end
+
+  sig {params(other: BlinkMemo).returns(T::Boolean)}
+  def eql?(other)
+    self.stone == other.stone && self.depth == other.depth
+  end
+
+  sig {returns(Integer)}
+  def hash
+    [@stone, @depth].hash
+  end
+end
+
+RECURSIVE_BLINK_CACHE = T.let({}, T::Hash[BlinkMemo, Integer])
+
+sig {params(stone: Integer, depth: Integer).returns(Integer)}
+def recursive_blink(stone, depth)
+  if depth == 0
+    return 1
+  end
+
+  memo = BlinkMemo.new(stone, depth)
+  if RECURSIVE_BLINK_CACHE.include?(memo)
+    return T.must(RECURSIVE_BLINK_CACHE[memo])
+  end
+
+  children = transform_stone(stone)
+  count = children.map{ |child| recursive_blink(child, depth - 1) }.sum
+  RECURSIVE_BLINK_CACHE[memo] = count
+  return count
 end
 
 options = {
+  :blinks => 25,
   :filename => 'input.txt'
 }
 
 OptionParser.new do |opts|
   opts.banner = 'Usage: d11.rb [options]'
+  opts.on('-b BLINKS', '--blinks=BLINKS', Integer, 'How many times are you blinking?')
   opts.on('-f FILENAME', '--filename=FILENAME', String, 'Which file to use?')
 end.parse!(into: options)
 
 stones = File.new(options[:filename]).readline.strip.split(' ').map(&:to_i)
-list = LinkedList::LinkedList.new
-stones.each do |stone|
-  list.append(stone)
-end
 
-(1..25).each do |count|
-  list = blink(list)
-end
-puts list.length
+total = stones.map{ |stone| recursive_blink(stone, options[:blinks]) }.sum
+puts total
