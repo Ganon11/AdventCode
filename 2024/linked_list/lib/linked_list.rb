0aec46cfda5eb962dbd67919f88f477bd4808dd1
Ganon11 <stark3@gmail.com>
2024-12-11 11:50:54 -0500

    chore: remove unused methods from linked list

--- 2024/linked_list/lib/linked_list.rb
+++ 2024/linked_list/lib/linked_list.rb
@@ -55,37 +55,6 @@ module LinkedList
       end
     end
 
-    sig {params(target: Node, value: T.untyped).void}
-    def append_after(target, value)
-      target_was_tail = (target == @tail)
-      new_node = Node.new(value)
-      new_node.next = target.next
-      new_node.prev = target
-      T.must(target.next).prev = new_node if target.next
-      target.next = new_node
-      @tail = new_node if target_was_tail
-    end
-
-    # sig {returns(T.nilable(Node))}
-    # def find_tail
-    #   # return nil if !@head
-    #   # node = @head
-    #   # return node if !node.next
-    #   # return node if !node.next while (node = node.next)
-    # end
-
-    sig {params(value: T.untyped).returns(T.nilable(Node))}
-    def find(value)
-      return nil if !@head
-      node = @head
-      return node if node.value == value
-      while (node = node.next)
-        return node if node.value == value
-      end
-
-      return nil
-    end
-
     sig {returns(Integer)}
     def length
       return 0 if !@head
@@ -96,34 +65,5 @@ module LinkedList
       end
       return length
     end
-
-    sig {returns(String)}
-    def to_s
-      return "" if !@head
-      node = @head
-      description = ""
-      while (node)
-        description += node.to_s
-        node = node.next
-      end
-      return description
-    end
   end
 end
-
-# l = LinkedList::LinkedList.new
-# l.append(1)
-# l.append(2)
-# l.append(3)
-
-# n = l.find(2)
-
-# puts "#{n}, next #{T.must(n).next}, prev #{T.must(n).prev}"
-
-# l.append_after(T.must(n), 7)
-
-# puts "#{n}, next #{T.must(n).next}, prev #{T.must(n).prev}"
-
-# n = l.find_tail
-
-# puts "#{n}, next #{T.must(n).next}, prev #{T.must(n).prev}"
